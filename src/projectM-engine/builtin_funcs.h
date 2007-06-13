f06d4ba58393eea959262727e748c44090a176d0
w1z7ard <w1z7ard@6778bc44-b910-0410-a7a0-be141de4315d>
2007-06-13 03:01:01 +0000

    - inlined builtin_funcs methods, removed builtin_funcs.cpp
    git-svn-id: https://projectm.svn.sourceforge.net/svnroot/projectm/trunk@159 6778bc44-b910-0410-a7a0-be141de4315d

--- src/projectM-engine/builtin_funcs.h
+++ src/projectM-engine/builtin_funcs.h
@@ -24,7 +24,7 @@
  * $Log$
  */
 
-/* Wrappers for all the builtin functions 
+/* Wrappers for all the builtin functions
    The arg_list pointer is a list of floats. Its
    size is equal to the number of arguments the parameter
    takes */
@@ -33,36 +33,187 @@
 #define _BUILTIN_FUNCS_H
 
 #include "projectM.h"
- 
-inline float below_wrapper(float * arg_list);
-inline float above_wrapper(float * arg_list);
-inline float equal_wrapper(float * arg_list);
-inline float if_wrapper(float * arg_list);
-inline float bnot_wrapper(float * arg_list);
-inline float rand_wrapper(float * arg_list);
-inline float bor_wrapper(float * arg_list);
-inline float band_wrapper(float * arg_list);
-inline float sigmoid_wrapper(float * arg_list);
-inline float max_wrapper(float * arg_list);
-inline float min_wrapper(float * arg_list);
-inline float sign_wrapper(float * arg_list);
-inline float sqr_wrapper(float * arg_list);
-inline float int_wrapper(float * arg_list);
-inline float nchoosek_wrapper(float * arg_list);
-inline float sin_wrapper(float * arg_list);
-inline float cos_wrapper(float * arg_list);
-inline float tan_wrapper(float * arg_list);
-inline float fact_wrapper(float * arg_list);
-inline float asin_wrapper(float * arg_list);
-inline float acos_wrapper(float * arg_list);
-inline float atan_wrapper(float * arg_list);
-inline float atan2_wrapper(float * arg_list);
 
-inline float pow_wrapper(float * arg_list);
-inline float exp_wrapper(float * arg_list);
-inline float abs_wrapper(float * arg_list);
-inline float log_wrapper(float *arg_list);
-inline float log10_wrapper(float * arg_list);
-inline float sqrt_wrapper(float * arg_list);
+
+/* Values to optimize the sigmoid function */
+#define R  32767
+#define RR 65534
+
+
+
+inline float int_wrapper(float * arg_list) {
+
+return floor(arg_list[0]);
+
+}
+
+
+inline float sqr_wrapper(float * arg_list) {
+
+return pow(2, arg_list[0]);
+}
+
+
+inline float sign_wrapper(float * arg_list) {
+
+return -arg_list[0];
+}
+
+inline float min_wrapper(float * arg_list) {
+
+if (arg_list[0] > arg_list[1])
+return arg_list[1];
+
+return arg_list[0];
+}
+
+inline float max_wrapper(float * arg_list) {
+
+if (arg_list[0] > arg_list[1])
+return arg_list[0];
+
+return arg_list[1];
+}
+
+/* consult your AI book */
+inline float sigmoid_wrapper(float * arg_list) {
+return (RR / (1 + exp( -(((float)(arg_list[0])) * arg_list[1]) / R) - R));
+}
+
+
+inline float bor_wrapper(float * arg_list) {
+
+return (float)((int)arg_list[0] || (int)arg_list[1]);
+}
+
+inline float band_wrapper(float * arg_list) {
+return (float)((int)arg_list[0] && (int)arg_list[1]);
+}
+
+inline float bnot_wrapper(float * arg_list) {
+return (float)(!(int)arg_list[0]);
+}
+
+inline float if_wrapper(float * arg_list) {
+
+if ((int)arg_list[0] == 0)
+return arg_list[2];
+return arg_list[1];
+}
+
+
+inline float rand_wrapper(float * arg_list) {
+float l=1;
+
+//  printf("RAND ARG:(%d)\n", (int)arg_list[0]);
+if ((int)arg_list[0] > 0)
+l = (float)((rand()) % ((int)arg_list[0]));
+//printf("VAL: %f\n", l);
+return l;
+}
+
+inline float equal_wrapper(float * arg_list) {
+	return (arg_list[0] == arg_list[1]);
+}
+
+
+inline float above_wrapper(float * arg_list) {
+
+return (arg_list[0] > arg_list[1]);
+}
+
+
+inline float below_wrapper(float * arg_list) {
+
+return (arg_list[0] < arg_list[1]);
+}
+
+inline float sin_wrapper(float * arg_list) {
+return (sin (arg_list[0]));
+}
+
+
+inline float cos_wrapper(float * arg_list) {
+return (cos (arg_list[0]));
+}
+
+inline float tan_wrapper(float * arg_list) {
+return (tan(arg_list[0]));
+}
+
+inline float asin_wrapper(float * arg_list) {
+return (asin (arg_list[0]));
+}
+
+inline float acos_wrapper(float * arg_list) {
+return (acos (arg_list[0]));
+}
+
+inline float atan_wrapper(float * arg_list) {
+return (atan (arg_list[0]));
+}
+
+inline float atan2_wrapper(float * arg_list) {
+return (atan2 (arg_list[0], arg_list[1]));
+}
+
+inline float pow_wrapper(float * arg_list) {
+return (pow (arg_list[0], arg_list[1]));
+}
+
+inline float exp_wrapper(float * arg_list) {
+return (exp(arg_list[0]));
+}
+
+inline float abs_wrapper(float * arg_list) {
+return (fabs(arg_list[0]));
+}
+
+inline float log_wrapper(float* arg_list) {
+return (log (arg_list[0]));
+}
+
+inline float log10_wrapper(float * arg_list) {
+return (log10 (arg_list[0]));
+}
+
+inline float sqrt_wrapper(float * arg_list) {
+return (sqrt (arg_list[0]));
+}
+
+
+inline float nchoosek_wrapper(float * arg_list) {
+unsigned long cnm = 1UL;
+int i, f;
+int n, m;
+
+n = (int)arg_list[0];
+m = (int)arg_list[1];
+
+if (m*2 >n) m = n-m;
+for (i=1 ; i <= m; n--, i++)
+{
+if ((f=n) % i == 0)
+f   /= i;
+else  cnm /= i;
+cnm *= f;
+}
+return (float)cnm;
+}
+
+
+inline float fact_wrapper(float * arg_list) {
+
+
+int result = 1;
+
+int n = (int)arg_list[0];
+
+while (n > 1) {
+result = result * n;
+n--;
+}
+return (float)result;
+}
 
 #endif /** !_BUILTIN_FUNCS_H */
