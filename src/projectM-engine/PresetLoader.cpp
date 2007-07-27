11076ac56b5080407c17a4848ca03917b6983769
w1z7ard <w1z7ard@6778bc44-b910-0410-a7a0-be141de4315d>
2007-07-27 05:34:41 +0000

    it runs and crashes somewhere interesting (PresetFrameIO?) with debug symbols working
    git-svn-id: https://projectm.svn.sourceforge.net/svnroot/projectm/personal/carm/dev-1.0@273 6778bc44-b910-0410-a7a0-be141de4315d

--- src/projectM-engine/PresetLoader.cpp
+++ src/projectM-engine/PresetLoader.cpp
@@ -1,7 +1,7 @@
 //
 // C++ Implementation: PresetLoader
 //
-// Description: 
+// Description:
 //
 //
 // Author: Carmelo Piccione <carmelo.piccione@gmail.com>, (C) 2007
@@ -13,7 +13,8 @@
 #include "Preset.hpp"
 #include <iostream>
 #include <sstream>
-extern "C" {
+extern "C"
+{
 #include <errno.h>
 #include <dirent.h>
 }
@@ -23,89 +24,111 @@ extern "C" {
 const std::string PresetLoader::PROJECTM_FILE_EXTENSION(".prjm");
 const std::string PresetLoader::MILKDROP_FILE_EXTENSION(".milk");
 
-PresetLoader::PresetLoader(std::string dirname) :m_dirname(dirname)
+PresetLoader::PresetLoader(std::string dirname) :m_dirname(dirname), m_dir(0)
 {
-	// Do one scan
-	rescan();
+  // Do one scan
+  rescan();
 }
 
-PresetLoader::~PresetLoader() {}
-
-void PresetLoader::setScanDirectory(std::string dirname) {
-	m_dirname = dirname;			
+PresetLoader::~PresetLoader()
+{
+  if (m_dir)
+    closedir(m_dir);
 }
 
-
-void PresetLoader::rescan() {
-
-// Clear the directory entry collection
-m_entries.clear();
-
-// If directory already opened, close it first
- if (m_dir) {
-	closedir(m_dir);
- } 
-
-// Allocate a new a stream given the current directory name
-if ((m_dir = opendir(m_dirname.c_str())) == NULL) {
-	handleDirectoryError();
+void PresetLoader::setScanDirectory(std::string dirname)
+{
+  m_dirname = dirname;
 }
 
 
-std::ostringstream out;
+void PresetLoader::rescan()
+{
+  std::cerr << "Rescanning..." << std::endl;
 
-struct dirent * dir_entry;
-while ((dir_entry = readdir(m_dir)) != NULL) {
+  // Clear the directory entry collection
+  m_entries.clear();
 
-	// Convert char * to friendly string
-	std::string filename(dir_entry->d_name);
-	
-	// Verify extension is projectm or milkdrop
-	if (filename.rfind(PROJECTM_FILE_EXTENSION) <= 0
-		|| filename.rfind(MILKDROP_FILE_EXTENSION) <= 0)
-		continue;
+  std::cerr << "cleared!" << std::endl;
 
-	// Create full path name
-	out << m_dirname << PATH_SEPARATOR << filename;
+  // If directory already opened, close it first
+  if (m_dir)
+  {
+  std::cerr << "m_dir" << std::endl;
+    closedir(m_dir);
+    m_dir = 0;
+  }
 
-	// Add to our directory entry collection
-	m_entries.push_back(out.str());
+  std::cerr << "opening " << m_dirname << std::endl;
+  // Allocate a new a stream given the current directory name
+  if ((m_dir = opendir(m_dirname.c_str())) == NULL)
+  {
+    handleDirectoryError();
+    abort();
+  }
 
-	// We can now toss out the directory entry struct
-	free(dir_entry);
+  std::ostringstream out;
+
+  struct dirent * dir_entry;
+  std::cerr << "iterating " << m_dirname << std::endl;
+  while ((dir_entry = readdir(m_dir)) != NULL)
+  {
+
+    // Convert char * to friendly string
+    std::string filename(dir_entry->d_name);
+
+    // Verify extension is projectm or milkdrop
+    if (filename.rfind(PROJECTM_FILE_EXTENSION) <= 0
+        || filename.rfind(MILKDROP_FILE_EXTENSION) <= 0)
+      continue;
+
+    // Create full path name
+    out << m_dirname << PATH_SEPARATOR << filename;
+
+    // Add to our directory entry collection
+    m_entries.push_back(out.str());
+
+    // the directory entry struct is free elsewhere
+  }
+
 }
 
+std::auto_ptr<Preset> PresetLoader::loadPreset(unsigned int index, const PresetInputs & presetInputs, PresetOutputs & presetOutputs) const
+{
+
+  // Check that index isn't insane
+  assert(index >= 0);
+  assert(index < m_entries.size());
+
+  // Return a new auto pointer to a present
+  return std::auto_ptr<Preset>(new Preset(m_entries[index], presetInputs, presetOutputs));
 }
 
-std::auto_ptr<Preset> PresetLoader::loadPreset(unsigned int index, const PresetInputs & presetInputs, PresetOutputs & presetOutputs) const {
-	
-	// Check that index isn't insane
-	assert(index >= 0);
-	assert(index < m_entries.size());
-
-	// Return a new auto pointer to a present
-	return std::auto_ptr<Preset>(new Preset(m_entries[index], presetInputs, presetOutputs));
-}
 
 
-
-void PresetLoader::handleDirectoryError() {	
-	switch (errno) {
-		case ENOENT:
-			break;		
-		case ENOMEM:
-			abort();
-		case ENOTDIR:
-			break;
-		case ENFILE:
-			std::cerr << "[PresetLoader] Your system has reached its open file limit. Giving up..." << std::endl;
-			abort();
-		case EMFILE:
-			std::cerr << "[PresetLoader] too many files in use by projectM! Bailing!" << std::endl;
-			abort();
-		case EACCES:
-			break;
-		default:
-			break;
-	}
+void PresetLoader::handleDirectoryError()
+{
+  switch (errno)
+  {
+  case ENOENT:
+    std::cerr << "[PresetLoader] ENOENT error. \"man fopen\" for more info." << std::endl;
+    break;
+  case ENOMEM:
+    std::cerr << "[PresetLoader] out of memory! Are you running Windows?" << std::endl;
+    abort();
+  case ENOTDIR:
+    std::cerr << "[PresetLoader] directory specified is not a preset directory! Cannot continue." << std::endl;
+    break;
+  case ENFILE:
+    std::cerr << "[PresetLoader] Your system has reached its open file limit. Giving up..." << std::endl;
+    abort();
+  case EMFILE:
+    std::cerr << "[PresetLoader] too many files in use by projectM! Bailing!" << std::endl;
+    abort();
+  case EACCES:
+    std::cerr << "[PresetLoader] permissions issue reading the specified preset directory." << std::endl;
+    break;
+  default:
+    break;
+  }
 }
