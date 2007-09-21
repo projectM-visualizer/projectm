c46ed203e5d1a4bb4e2aa243777db28c774dcb6d
w1z7ard <w1z7ard@6778bc44-b910-0410-a7a0-be141de4315d>
2007-09-21 04:19:19 +0000

    woops- forgot ;
    git-svn-id: https://projectm.svn.sourceforge.net/svnroot/projectm/trunk@465 6778bc44-b910-0410-a7a0-be141de4315d

--- src/projectM-engine/RingBuffer.hpp
+++ src/projectM-engine/RingBuffer.hpp
@@ -7,7 +7,7 @@ class RingBuffer {
 public:
 
 //	static const unsigned long RING_BUFFER_SIZE = 32768;
-	static const unsigned long RING_BUFFER_SIZE = 16384.
+	static const unsigned long RING_BUFFER_SIZE = 16384;
 private:
 
     kind buffer[RING_BUFFER_SIZE]; 
