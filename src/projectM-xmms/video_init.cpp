9a09b5ed41c4ff4cfa16853ad2eda71aa8956fc4
w1z7ard <w1z7ard@6778bc44-b910-0410-a7a0-be141de4315d>
2007-10-05 01:57:24 +0000

    - removed all print outs noticed in xmms plugin - might have finally fixed start up bug by putting SDL_Init in root thread
    git-svn-id: https://projectm.svn.sourceforge.net/svnroot/projectm/trunk@513 6778bc44-b910-0410-a7a0-be141de4315d

--- src/projectM-xmms/video_init.cpp
+++ src/projectM-xmms/video_init.cpp
@@ -10,7 +10,7 @@
 #include <GL/gl.h>
 #include <GL/glu.h>
 #include "video_init.h"
-
+#include <iostream>
 extern SDL_Surface *screen;
 extern int texsize;
 
@@ -41,15 +41,8 @@ void init_display(int w, int h, int *fvw, int *fvh, int f)
   int bpp = 0;
   /* Flags we will pass into SDL_SetVideoMode. */
   int flags = 0;
-  /* First, initialize SDL's video subsystem. */
-  if( SDL_Init( SDL_INIT_VIDEO | SDL_INIT_TIMER ) < 0 ) {
-    /* Failed, exit. */
-    fprintf( stderr, "Video initialization failed: %s\n",
-             SDL_GetError( ) );
-    //projectM_vtable.disable_plugin (&projectM_vtable);
-    return;
-    
-  }
+
+
   /* Let's get some video information. */
   info = SDL_GetVideoInfo( );
   if( !info ) {
@@ -60,12 +53,10 @@ void init_display(int w, int h, int *fvw, int *fvh, int f)
     return;
   }
   
-  printf("Screen Resolution: %d x %d\n", info->current_w, info->current_h);
+//  printf("Screen Resolution: %d x %d\n", info->current_w, info->current_h);
   *fvw = info->current_w;
   *fvh = info->current_h;
-  printf("set fvw and fvw\n");
   bpp = info->vfmt->BitsPerPixel;
-  printf("set bpp\n");
   //SDL_GL_SetAttribute( SDL_GL_RED_SIZE, 8 );
   //SDL_GL_SetAttribute( SDL_GL_GREEN_SIZE, 8 );
   //SDL_GL_SetAttribute( SDL_GL_BLUE_SIZE, 8 );
@@ -76,13 +67,10 @@ void init_display(int w, int h, int *fvw, int *fvh, int f)
   SDL_GL_SetAttribute( SDL_GL_ALPHA_SIZE, 8 );
   SDL_GL_SetAttribute( SDL_GL_DEPTH_SIZE, 16 );
   SDL_GL_SetAttribute( SDL_GL_DOUBLEBUFFER, 1 );
-  printf("set gl attributes\n");
   if (f==0)
      flags = SDL_OPENGL|SDL_HWSURFACE|SDL_RESIZABLE;
   else flags = SDL_OPENGL|SDL_HWSURFACE|SDL_FULLSCREEN;
- printf("set gl attributes2\n");
-  screen= SDL_SetVideoMode( w, h, bpp, flags ) ;
- printf("set screen\n");
+ screen= SDL_SetVideoMode( w, h, bpp, flags ) ;
   if(screen == 0 ) {
     /* 
      * This could happen for a variety of reasons,
@@ -97,7 +85,6 @@ void init_display(int w, int h, int *fvw, int *fvh, int f)
     
   }
   
-  printf("video init done\n");
   // setup_opengl(w,h);
   //gluOrtho2D(0, w, 0, h);
 }
