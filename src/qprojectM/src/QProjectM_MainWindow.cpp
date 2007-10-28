f6ec80d9bdcb0e2fb764522c84e031951588c809
w1z7ard <w1z7ard@6778bc44-b910-0410-a7a0-be141de4315d>
2007-10-28 15:39:28 +0000

    merge from dev branch (r583:HEAD) - qprojectM lib, qprojectM-jack added - updates to projectm engine for preset managment / callbacksO
    git-svn-id: https://projectm.svn.sourceforge.net/svnroot/projectm/trunk@630 6778bc44-b910-0410-a7a0-be141de4315d

---
+++ src/qprojectM/src/QProjectM_MainWindow.cpp
@@ -0,0 +1,289 @@
+/***************************************************************************
+ *   Copyright (C) 2007 by carm   *
+ *   carm@localhost   *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+
+#include <QtGui>
+#include "QProjectM_MainWindow.hpp"
+#include "QProjectMFileDialog.hpp"
+
+#include <QTextStream>
+#include <QCloseEvent>
+#include <QFileDialog>
+
+QProjectM_MainWindow::QProjectM_MainWindow(const std::string & config_file)
+:m_QProjectMFileDialog(new QProjectMFileDialog(this))
+{
+
+      ui.setupUi(this);
+
+      m_QProjectMWidget = new QProjectMWidget(config_file, this);
+	
+      m_timer = new QTimer(this);
+      connect(m_timer, SIGNAL(timeout()), m_QProjectMWidget, SLOT(updateGL()));
+
+      connect(ui.lockPresetCheckBox, SIGNAL(stateChanged(int)),
+	m_QProjectMWidget, SLOT(setPresetLock(int)));
+
+      connect(m_QProjectMWidget, SIGNAL(projectM_Initialized()), this, SLOT(postProjectM_Initialize()));
+
+        
+      ui.tableView->setVerticalHeader(0);
+
+      ui.tableView->setModel(&playlistModel);
+
+      m_QProjectMWidget->makeCurrent();
+      m_QProjectMWidget->setFocus();
+
+      m_timer->start(0);
+
+      setCentralWidget(m_QProjectMWidget);
+	
+      createActions();
+      createMenus();
+      createToolBars();
+      createStatusBar();
+      readSettings();
+      ui.presetPlayListDockWidget->hide();
+       
+
+}
+
+QProjectM * QProjectM_MainWindow::getQProjectM() {
+	return m_QProjectMWidget->getQProjectM();
+}
+
+void QProjectM_MainWindow::updatePlaylistSelection(bool hardCut, unsigned int index) {
+	
+	if (hardCut)
+		statusBar()->showMessage(tr("*** Hard cut ***") , 2000);
+	else
+		statusBar()->showMessage(tr("*** Soft cut ***") , 2000);
+
+	ui.tableView->selectRow(index);
+
+
+}
+
+void QProjectM_MainWindow::selectPlaylistItem(const QModelIndex & index) {
+	getQProjectM()->selectPreset(index.row());
+}
+
+
+void QProjectM_MainWindow::postProjectM_Initialize() {
+	refreshPlaylist();
+  
+        connect(m_QProjectMWidget->getQProjectM(), SIGNAL(presetSwitchedSignal(bool,unsigned int)), this, SLOT(updatePlaylistSelection(bool,unsigned int)));
+
+	
+        connect(ui.tableView, SIGNAL(activated(const QModelIndex &)), 
+		this, SLOT(selectPlaylistItem(const QModelIndex &)));
+
+}
+
+void QProjectM_MainWindow::keyReleaseEvent ( QKeyEvent * e )  {
+
+	QModelIndex modelIndex;
+	switch (e->key()) {
+	case Qt::Key_L:
+		if (ui.lockPresetCheckBox->checkState() == Qt::Checked)
+			ui.lockPresetCheckBox->setCheckState(Qt::Unchecked);
+		else
+			ui.lockPresetCheckBox->setCheckState(Qt::Checked);
+	
+		// the projectM widget handles the actual lock
+		//e->ignore();			
+		//m_QProjectMWidget->keyReleaseEvent(e);
+		return;
+
+	case Qt::Key_F:			
+		this->setWindowState(this->windowState() ^ Qt::WindowFullScreen);
+		return;
+
+	case Qt::Key_M:
+
+		if (ui.presetPlayListDockWidget->isVisible()) {
+			ui.presetPlayListDockWidget->hide();
+		} else
+			ui.presetPlayListDockWidget->show();
+	
+		if (menuBar()->isVisible()) {
+			menuBar()->hide();
+			m_QProjectMWidget->setFocus();
+		}
+		else {
+		   menuBar()->show();
+		}
+
+		if (statusBar()->isVisible()) {
+			statusBar()->hide();
+		}
+		else {
+		   statusBar()->show();
+		}
+		return;	
+
+	case Qt::Key_R:
+		//modelIndex.selectRandom()
+		//modelIndex = QModelIndex(0,0,0);
+		//selectPlaylistItem(modelIndex);
+		//updatePlaylistSelection(true, modelIndex.row());
+		return;
+	default:
+		//m_QProjectMWidget->keyReleaseEvent(e);
+		break;//e->ignore();	
+	}
+	
+}
+
+void QProjectM_MainWindow::closeEvent(QCloseEvent *event)
+{
+}
+
+
+void QProjectM_MainWindow::open()
+{
+
+
+            if ( m_QProjectMFileDialog->exec()) {
+		const QStringList & files = m_QProjectMFileDialog->selectedFiles();
+
+		for (QStringList::const_iterator pos = files.begin(); 
+			pos != files.end(); ++pos) {
+			if (*pos != "")
+				loadFile(*pos);
+		}
+	    }
+
+}
+
+
+void QProjectM_MainWindow::refreshPlaylist() {
+
+	playlistModel.clear();
+
+	for (unsigned int i = 0; i < getQProjectM()->getPlayListSize(); i++) {
+		
+		const std::string & presetURL = getQProjectM()->getPresetURL(i);
+		const std::string & presetName = getQProjectM()->getPresetName(i);
+
+		QList<QStandardItem*> items;
+		items.append(new QStandardItem(QString(presetName.c_str())));
+		items[0]->setEditable(false);
+
+		playlistModel.appendRow(items);
+	}
+
+
+    QHeaderView * hHeader = new QHeaderView(Qt::Horizontal, this);
+    QHeaderView * vHeader = new QHeaderView(Qt::Vertical, this);
+
+    hHeader->setClickable(true);
+    hHeader->setSortIndicatorShown(true);
+    hHeader->setSortIndicator(1, Qt::AscendingOrder);
+    hHeader->setStretchLastSection(true);
+
+	ui.tableView->setVerticalHeader(vHeader);	
+	ui.tableView->setHorizontalHeader(hHeader);
+
+	playlistModel.setHeaderData(0, Qt::Horizontal, tr("Preset"));//, Qt::DisplayRole);
+
+	vHeader->hide();
+
+	//playlistModel.setHeaderData(0, Qt::Vertical, 1000, Qt::SizeHintRole);
+	//playlistModel.setHeaderData(1, Qt::Horizontal, tr("Rating"));//, Qt::DisplayRole);
+	//playlistModel.setHeaderData(2, Qt::Horizontal, tr("Preset"));//, Qt::DisplayRole);
+	
+	
+}
+
+void QProjectM_MainWindow::about()
+{
+      QMessageBox::about(this, tr("About QProjectM"),
+            tr("<b>QProjectM</b> provides useful gui extensions to the projectM core library."));
+}
+
+
+void QProjectM_MainWindow::createActions()
+{
+
+      connect(ui.actionExit, SIGNAL(triggered()), this, SLOT(close()));
+
+      connect(ui.actionAddPresets, SIGNAL(triggered()), this, SLOT(open()));
+
+      connect(ui.actionAbout_qprojectM, SIGNAL(triggered()), this, SLOT(about()));
+
+      //connect(ui.actionAbout_Qt, SIGNAL(triggered()), this, SLOT(aboutQt()));
+
+}
+
+void QProjectM_MainWindow::createMenus()
+{
+	ui.menuBar->hide();
+
+}
+
+void QProjectM_MainWindow::createToolBars()
+{ 
+}
+
+void QProjectM_MainWindow::createStatusBar()
+{
+      statusBar()->hide();
+      statusBar()->showMessage(tr("Welcome to qprojectM!"));
+}
+
+void QProjectM_MainWindow::readSettings()
+{
+      QSettings settings("Trolltech", "QProjectM");
+      QPoint pos = settings.value("pos", QPoint(200, 200)).toPoint();
+      QSize size = settings.value("size", QSize(1000, 600)).toSize();
+      resize(size);
+      move(pos);
+}
+
+void QProjectM_MainWindow::writeSettings()
+{
+      QSettings settings("Trolltech", "QProjectM");
+      settings.setValue("pos", pos());
+      settings.setValue("size", size());
+}
+
+
+
+void QProjectM_MainWindow::loadFile(const QString &fileName)
+{
+	QProjectM * projectM = this->getQProjectM();
+	unsigned int index = projectM->addPresetURL(fileName.toStdString(), strippedName(fileName).toStdString());
+
+	QList<QStandardItem*> items;
+	items.append(new QStandardItem(QString(projectM->getPresetName(index).c_str())));
+	items[0]->setEditable(false);
+
+	playlistModel.appendRow(items);
+
+}
+
+
+QString QProjectM_MainWindow::strippedName(const QString &fullFileName)
+{
+      return QFileInfo(fullFileName).fileName();
+}
+
+
---
+++ src/qprojectM/src/QProjectM_MainWindow.hpp
@@ -0,0 +1,251 @@
+/***************************************************************************
+ *   Copyright (C) 2007 by carm   *
+ *   carm@localhost   *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+
+#ifndef QPROJECTM_MAINWINDOW_H
+#define QPROJECTM_MAINWINDOW_H
+#define CONFIG_FILE "/share/projectM/config.inp"
+
+#include "ui_QProjectM_MainWindow.h"
+
+#include <QMainWindow>
+#include <QCloseEvent>
+#include <cassert>
+#include "libprojectM/projectM.hpp"
+
+#include <QGLWidget>
+#include <QStandardItemModel>
+
+class QAction;
+class QMenu;
+class QProjectMFileDialog;
+
+#include <iostream>
+
+
+class QProjectM : public QObject, public projectM {
+	Q_OBJECT
+
+	public:
+		 QProjectM(const std::string & config_file):projectM(config_file) {} 
+        
+		 
+		 void presetSwitchedEvent(bool hardCut, unsigned int index) const {
+			presetSwitchedSignal(hardCut, index);
+		 }
+	signals:
+		void presetSwitchedSignal(bool hardCut, unsigned int index) const;
+	public slots:
+			
+};
+
+class QProjectMWidget : public QGLWidget
+ {
+     Q_OBJECT        // must include this if you use Qt signals/slots
+
+ public:
+     QProjectMWidget(const std::string & _config_file, QWidget *parent)
+         : QGLWidget(parent), config_file(_config_file), m_projectM(0) {}
+     ~QProjectMWidget() { if (m_projectM) delete(m_projectM); }
+
+     QProjectM * getQProjectM() { return m_projectM; }
+
+ public slots:
+     void setPresetLock(int state) {
+		m_projectM->setPresetLock((bool)state);
+     }
+  signals:
+	void projectM_Initialized();
+
+ private:
+	std::string config_file;
+	QProjectM * m_projectM;
+ protected:
+
+void keyReleaseEvent ( QKeyEvent * e )  {
+	
+	projectMKeycode pkey;
+    bool ignore = false;
+    switch (e->key()) {
+	case Qt::Key_F4:
+		pkey =  PROJECTM_K_F4;
+		break;
+	case Qt::Key_F3:
+		pkey =  PROJECTM_K_F3;
+		break;
+	case Qt::Key_F2:
+		pkey =  PROJECTM_K_F2;
+		break;
+	case Qt::Key_F1:
+		pkey =  PROJECTM_K_F1;
+		break;
+	case Qt::Key_R:
+		pkey =  PROJECTM_K_r;
+		break;
+	case Qt::Key_L:
+		pkey =  PROJECTM_K_l;
+		ignore = true;
+		break;	
+	case Qt::Key_N:
+  		pkey =  PROJECTM_K_n;
+		break;
+	case Qt::Key_P:	
+		pkey =  PROJECTM_K_p;
+		break;		
+	default:
+		e->ignore();
+		return;
+    }
+       projectMModifier modifier;
+
+	//std::cerr << "HERE with key " << e->key() << std::endl;
+	m_projectM->key_handler(PROJECTM_KEYDOWN, pkey, modifier);
+	if (ignore)
+		e->ignore();
+
+	
+     }
+
+     void initializeGL()
+     {
+	this->m_projectM = new QProjectM(config_file);
+	projectM_Initialized();
+     }
+
+     void resizeGL(int w, int h)
+     {
+         // setup viewport, projection etc.:
+	/// @bug is setup necessary on resize or should just be in initializeGL() ?
+	setup_opengl(w,h);
+	m_projectM->projectM_resetGL(  w, h ); 
+     }
+
+     void paintGL()
+     {
+	assert(m_projectM);
+   	m_projectM->renderFrame();
+     }
+
+   private:
+
+ void setup_opengl( int w, int h )
+{
+   
+    /* Our shading model--Gouraud (smooth). */
+     glShadeModel( GL_SMOOTH);
+    /* Culling. */
+    //    glCullFace( GL_BACK );
+    //    glFrontFace( GL_CCW );
+    //    glEnable( GL_CULL_FACE );
+    /* Set the clear color. */
+    glClearColor( 0, 0, 0, 0 );
+    /* Setup our viewport. */
+     glViewport( 0, 0, w, h );
+    /*
+     * Change to the projection matrix and set
+     * our viewing volume.
+     */
+    glMatrixMode(GL_TEXTURE);
+    glLoadIdentity();
+    
+    //    gluOrtho2D(0.0, (GLfloat) width, 0.0, (GLfloat) height);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();  
+   
+    //    glFrustum(0.0, height, 0.0,width,10,40);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
+glDrawBuffer(GL_BACK); 
+  glReadBuffer(GL_BACK); 
+  glEnable(GL_BLEND); 
+
+     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA); 
+     // glBlendFunc(GL_SRC_ALPHA, GL_ONE); 
+  glEnable(GL_LINE_SMOOTH);
+  glEnable(GL_POINT_SMOOTH);
+  glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+//   glClear(GL_COLOR_BUFFER_BIT);
+ 
+  // glCopyTexImage2D(GL_TEXTURE_2D,0,GL_RGB,0,0,texsize,texsize,0);
+  //glCopyTexSubImage2D(GL_TEXTURE_2D,0,0,0,0,0,texsize,texsize);
+   glLineStipple(2, 0xAAAA);
+  
+    
+}
+
+
+ };
+
+class QProjectM_MainWindow:public QMainWindow
+{
+      Q_OBJECT
+
+public:
+
+      QProjectM_MainWindow(const std::string & config_file);
+      ~QProjectM_MainWindow() {};
+     void keyReleaseEvent ( QKeyEvent * e );
+      QProjectM * getQProjectM();
+
+    void refreshPlaylist();
+
+    
+protected:
+      void closeEvent(QCloseEvent *event);
+
+private slots:
+      void open();
+      void about();
+      void postProjectM_Initialize();
+      void updatePlaylistSelection(bool hardCut, unsigned int index);
+      void selectPlaylistItem(const QModelIndex & index);
+
+private:
+	
+      QStandardItemModel playlistModel;
+      Ui::qProjectM_MainWindow ui;
+      QTimer * m_timer;
+      void createActions();
+      void createMenus();
+      void createToolBars();
+      void createStatusBar();
+      void readSettings();
+      void writeSettings();
+      bool maybeSave();
+      void loadFile(const QString &fileName);
+      QString strippedName(const QString &fullFileName);
+
+      QProjectMWidget * m_QProjectMWidget;
+
+      QMenu *fileMenu;
+      QMenu *helpMenu;
+      QToolBar *fileToolBar;
+
+      QAction *openAct;;
+      QAction *exitAct;
+      QAction *aboutAct;
+      QAction *aboutQtAct;
+
+
+      QProjectMFileDialog * m_QProjectMFileDialog;
+};
+
+#endif
