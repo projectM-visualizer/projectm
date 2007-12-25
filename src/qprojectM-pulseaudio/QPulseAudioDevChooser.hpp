3953eee74be46fa039ee46024a35c6243e769d45
w1z7ard <w1z7ard@6778bc44-b910-0410-a7a0-be141de4315d>
2007-12-25 02:01:17 +0000

    more work on dev chooser. mmm wine.
    git-svn-id: https://projectm.svn.sourceforge.net/svnroot/projectm/trunk@740 6778bc44-b910-0410-a7a0-be141de4315d

--- src/qprojectM-pulseaudio/QPulseAudioDevChooser.hpp
+++ src/qprojectM-pulseaudio/QPulseAudioDevChooser.hpp
@@ -3,24 +3,23 @@
 #include "ui_PulseDeviceChooserDialog.h"
 class QDialog;
 
+
 class QPulseAudioDevChooser : public QDialog, public Ui::pulseDeviceChooserDialog {
 
 	Q_OBJECT
 
 	public:
+	typedef QVector<QString> SourceContainer;
 //		QPulseAudioDevChooser(QWidget * parent = 0, Qt::WindowFlags f = 0 ) ;
 //QPulseAudioDevChooser::QPulseAudioDevChooser(QWidget * parent = 0, Qt::WindowFlags f = 0 ) : QDialog(parent, f) {}
 
 	public slots:
-		//void updateDevice(int index, QString name);
-		//void removeDevice(int index);
-		//void refreshDevices();
+		void updateDevice(int index, const QString & name);
+		void removeDevice(int index);
+		void refreshDevices(SourceContainer::const_iterator beginPos,SourceContainer::const_iterator endPos);
 
 	private slots:
-		void saveConfig();
-
-
-
-
+		void readSettings();
+		void writeSettings();
 };
 #endif
