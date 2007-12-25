2c850d4642b2fa22eb1369b5053dedee2923098b
w1z7ard <w1z7ard@6778bc44-b910-0410-a7a0-be141de4315d>
2007-12-25 02:01:17 +0000

    more work on dev chooser. mmm wine.
    git-svn-id: https://projectm.svn.sourceforge.net/svnroot/projectm/trunk@740 6778bc44-b910-0410-a7a0-be141de4315d

--- src/qprojectM-pulseaudio/QPulseAudioDevChooser.cpp
+++ src/qprojectM-pulseaudio/QPulseAudioDevChooser.cpp
@@ -1,6 +1,40 @@
 #include "QPulseAudioDevChooser.hpp"
+#include <QSettings>
+
+void QPulseAudioDevChooser::writeSettings() {
+	
+	QSettings settings ( "projectM", "qprojectM-pulseaudio");
+	settings.setValue ( "tryFirstAvailablePlaybackMonitor", 
+		this->tryFirstPlayBackMonitorCheckBox->checkState() == Qt::Checked);	
+	
+}
 
 
-void QPulseAudioDevChooser::saveConfig() {
+void QPulseAudioDevChooser::readSettings() {
+	
+QSettings settings ( "projectM", "qprojectM-pulseaudio");
 
+this->tryFirstPlayBackMonitorCheckBox->setCheckState
+	(settings.value("tryFirstAvailablePlaybackMonitor", true).toBool() ? Qt::Checked : Qt::Unchecked);
+}
+
+void QPulseAudioDevChooser::updateDevice(int index, const QString & name) {
+	this->deviceListWidget->item(index)->setText(name);
+}
+
+void QPulseAudioDevChooser::removeDevice(int index) {
+	
+}
+
+void QPulseAudioDevChooser::refreshDevices
+	(SourceContainer::const_iterator beginPos, SourceContainer::const_iterator endPos) {
+
+	SourceContainer::const_iterator pos = beginPos;
+
+	this->deviceListWidget->clear();
+
+	while (pos != endPos) {
+		this->deviceListWidget->addItem(*pos);
+		++pos;
+	}
 }
