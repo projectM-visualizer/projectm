0ef52df5e749ba8175883d774afbef46084a2def
w1z7ard <w1z7ard@6778bc44-b910-0410-a7a0-be141de4315d>
2008-01-25 07:13:08 +0000

    - replace Ui::QProjectMConfigDialog to ::QProjectMConfigDialog - tested save and load behavior. partially implemented
    git-svn-id: https://projectm.svn.sourceforge.net/svnroot/projectm/trunk@772 6778bc44-b910-0410-a7a0-be141de4315d

--- src/qprojectM/src/QProjectMConfigDialog.cpp
+++ src/qprojectM/src/QProjectMConfigDialog.cpp
@@ -1,13 +1,17 @@
 #include "QProjectMConfigDialog.hpp"
+#include <QtDebug>
 
-QProjectMConfigDialog::QProjectMConfigDialog(const std::string & configFile, projectM & projectM, QWidget * parent = 0, Qt::WindowFlags f = 0 ) : QDialog(parent, f), _configFile(configFile), _projectM(projectM) {
+QProjectMConfigDialog::QProjectMConfigDialog(const std::string & configFile, projectM * projectM, QWidget * parent, Qt::WindowFlags f) : QDialog(parent, f), _configFile(configFile), _projectM(*projectM) {
 	
+	qDebug() << "!!!";
 	_ui.setupUi(this);
-	connect(_ui.buttonBox, SIGNAL(accept()), this, SLOT(saveConfig()));
+	connect(_ui.buttonBox, SIGNAL(accepted()), this, SLOT(saveConfig()));
+	loadConfig();
 }
 
 
 void QProjectMConfigDialog::saveConfig() {
+	qDebug() << "SAVE config";
 	
 	projectM::Settings settings = _projectM.settings();
 	
@@ -25,10 +29,34 @@ void QProjectMConfigDialog::saveConfig() {
 	settings.aspectCorrection = _ui.useAspectCorrectionCheckBox->checkState() == Qt::Checked;
 	settings.beatSensitivity = _ui.beatSensitivitySpinBox->value();
 		
-	_projectM.writeConfig(_configFile, settings);	
+	projectM::writeConfig(_configFile, settings);
+	
+	qDebug() << "save end";
 }
 
 
 void QProjectMConfigDialog::loadConfig() {
 	
+	qDebug() << "load config";
+	const projectM::Settings & settings = _projectM.settings();
+	
+	//settings.meshX = _ui.meshSizeXComboBox->itemData(_ui.meshSizeXComboBox->currentIndex()).toInt();
+	//settings.meshY = _ui.meshSizeYComboBox->itemData(_ui.meshSizeYComboBox->currentIndex()).toInt();
+	
+	_ui.titleFontPathLineEdit->setText(settings.titleFontURL.c_str());
+	_ui.menuFontPathLineEdit->setText(settings.menuFontURL.c_str());
+	
+	_ui.startupPlaylistLineEdit->setText(settings.presetURL.c_str());
+	_ui.useAspectCorrectionCheckBox->setCheckState(settings.aspectCorrection ? Qt::Checked : Qt::Unchecked);
+	_ui.maxFPSSpinBox->setValue(settings.fps);
+	_ui.beatSensitivitySpinBox->setValue(settings.beatSensitivity);
+	_ui.windowHeightSpinBox->setValue(settings.windowHeight);
+	_ui.windowWidthSpinBox->setValue(settings.windowWidth);
+	
+	//settings.textureSize = _ui.textureSizeComboBox->itemData(_ui.textureSizeComboBox->currentIndex()).toInt();
+	_ui.smoothPresetDurationSpinBox->setValue(settings.smoothPresetDuration);
+	_ui.presetDurationSpinBox->setValue(settings.presetDuration);
+	
+	qDebug() << "load config END";
+		
 }
