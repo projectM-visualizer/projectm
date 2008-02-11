89d2752334114333f63c6858451930e29be873ab
w1z7ard <w1z7ard@6778bc44-b910-0410-a7a0-be141de4315d>
2008-02-11 00:28:37 +0000

    deleted unused code
    git-svn-id: https://projectm.svn.sourceforge.net/svnroot/projectm/trunk@814 6778bc44-b910-0410-a7a0-be141de4315d

--- src/projectM-engine/RandomNumberGenerators.cpp
+++ src/projectM-engine/RandomNumberGenerators.cpp
@@ -3,16 +3,7 @@
 #include <cmath>
 
 namespace RandomNumberGenerators {
-
-double gaussian( double min = 0, double max=1, double sigma=1, double mean=.5)
-{
-	double random = (min + (max-min) * (double)rand()/RAND_MAX); //create random domain between [min,max]
-	double tmp = (random-mean)/sigma;
-	double gauss= exp(-tmp*tmp/2); //gaussian formula
-	
-	return gauss;
-}
-	
+7	
 	
 }
 
