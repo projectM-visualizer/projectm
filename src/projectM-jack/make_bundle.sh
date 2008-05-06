294ade0d49319f4471ccfc7f23d5fd1cba151744
w1z7ard <w1z7ard@6778bc44-b910-0410-a7a0-be141de4315d>
2008-05-06 00:51:52 +0000

    working bundle maker?
    git-svn-id: https://projectm.svn.sourceforge.net/svnroot/projectm/trunk@1009 6778bc44-b910-0410-a7a0-be141de4315d

--- src/projectM-jack/make_bundle.sh
+++ src/projectM-jack/make_bundle.sh
@@ -3,54 +3,53 @@
 ### Author: Carmelo Piccione (carmelo.piccione+projectM@gmail.com) ###
 
 # Path to installed qt framework on your machine
-PATH_TO_QT =  /Developer/SDKs/MacOSX10.4u.sdk/Library/Frameworks/
+PATH_TO_QT=/Developer/SDKs/MacOSX10.4u.sdk/Library/Frameworks/
 
 # the version of qt to bundle
-QT_VERSION = 4.3
+QT_VERSION=Current
 
 # The bundle top level directory
-APP = projectM-jack.app
+APP=projectM-jack.app
 
 ## [1] Create framework directory
-mkdir -f ${APP}/Contents/Frameworks
+mkdir ${APP}
+mkdir ${APP}/Contents
+mkdir ${APP}/Contents/Frameworks
+mkdir ${APP}/Contents/MacOs
 
+cp projectM-jack ${APP}/Contents/MAcOs/
 
 ## [2] Copy each qt framework into the bundle
-cp -R ${PATH_TO_QT}/QtCore.framework ${APP}/Contents/Frameworks
-cp -R ${PATH_TO_QT}/QtGui.framework ${APP}/Contents/Frameworks
-cp -R ${PATH_TO_QT}/QtXml.framework ${APP}/Contents/Frameworks
-cp -R ${PATH_TO_QT}/QtOpenGL.framework ${APP}/Contents/Frameworks
+cp -Rf ${PATH_TO_QT}/QtCore.framework ${APP}/Contents/Frameworks
+cp -Rf ${PATH_TO_QT}/QtGui.framework ${APP}/Contents/Frameworks
+cp -Rf ${PATH_TO_QT}/QtXml.framework ${APP}/Contents/Frameworks
+cp -Rf ${PATH_TO_QT}/QtOpenGL.framework ${APP}/Contents/Frameworks
 
 
 ## [3] Change id of each qt framework to specialized one for this bundle
-install_name_tool -id @executable_path/../Frameworks/QtCore.framework/Versions/${QT_VERSION}/QtCore \
-	 ${APP}/Contents/Frameworks/QtCore.framework/Versions/${QT_VERSION}/QtCore
+install_name_tool -id @executable_path/../Frameworks/QtCore.framework/Versions/${QT_VERSION}/QtCore ${APP}/Contents/Frameworks/QtCore.framework/Versions/${QT_VERSION}/QtCore
 
-install_name_tool -id @executable_path/../Frameworks/QtGui.framework/Versions/${QT_VERSION}/QtGui \
-	${APP}/Contents/Frameworks/QtGui.framework/Versions/${QT_VERSION}/QtGui
+install_name_tool -id @executable_path/../Frameworks/QtGui.framework/Versions/${QT_VERSION}/QtGui ${APP}/Contents/Frameworks/QtGui.framework/Versions/${QT_VERSION}/QtGui
 
-install_name_tool -id @executable_path/../Frameworks/QtXml.framework/Versions/${QT_VERSION}/QtXml \
-	${APP}/Contents/Frameworks/QtXml.framework/Versions/${QT_VERSION}/QtXml
+install_name_tool -id @executable_path/../Frameworks/QtXml.framework/Versions/${QT_VERSION}/QtXml ${APP}/Contents/Frameworks/QtXml.framework/Versions/${QT_VERSION}/QtXml
 	
-install_name_tool -id @executable_path/../Frameworks/QtOpenGL.framework/Versions/${QT_VERSION}/QtOpenGL \
-	${APP}/Contents/Frameworks/QtOpenGL.framework/Versions/${QT_VERSION}/QtOpenGL
-
+install_name_tool -id @executable_path/../Frameworks/QtOpenGL.framework/Versions/${QT_VERSION}/QtOpenGL ${APP}/Contents/Frameworks/QtOpenGL.framework/Versions/${QT_VERSION}/QtOpenGL
 
 ## [4] Change reference ids in projectM-jack to the local referred to id's
-install_name_tool -change ${PATH_TO_QT}/QtCore.framework/Versions/${QT_VERSION}/QtCore
-         @executable_path/../Frameworks/QtCore.framework/Versions/${QT_VERSION}/QtCore
+install_name_tool -change ${PATH_TO_QT}/QtCore.framework/Versions/${QT_VERSION}/QtCore \
+         @executable_path/../Frameworks/QtCore.framework/Versions/${QT_VERSION}/QtCore \
+         ${APP}/Contents/MacOs/projectM-jack
+
+install_name_tool -change ${PATH_TO_QT}/QtGui.framework/Versions/${QT_VERSION}/QtGui \
+         @executable_path/../Frameworks/QtGui.framework/Versions/${QT_VERSION}/QtGui \
          ${APP}/Contents/MacOs/projectM-jack
 		 
-install_name_tool -change ${PATH_TO_QT}/QtGui.framework/Versions/${QT_VERSION}/QtGui
-         @executable_path/../Frameworks/QtGui.framework/Versions/${QT_VERSION}/QtGui
+		 
+install_name_tool -change ${PATH_TO_QT}/QtXml.framework/Versions/${QT_VERSION}/QtXml \
+         @executable_path/../Frameworks/QtXml.framework/Versions/${QT_VERSION}/QtXml \
          ${APP}/Contents/MacOs/projectM-jack
 		 
-		 
-install_name_tool -change ${PATH_TO_QT}/QtXml.framework/Versions/${QT_VERSION}/QtXml
-         @executable_path/../Frameworks/QtXml.framework/Versions/${QT_VERSION}/QtXml
-         ${APP}/Contents/MacOs/projectM-jack
-		 
-install_name_tool -change ${PATH_TO_QT}/QtOpenGL.framework/Versions/${QT_VERSION}/QtOpenGL
-         @executable_path/../Frameworks/QtOpenGL.framework/Versions/${QT_VERSION}/QtOpenGL
-         ${APP}/Contents/MacOs/projectM-jack
+install_name_tool -change ${PATH_TO_QT}/QtOpenGL.framework/Versions/${QT_VERSION}/QtOpenGL \
+         @executable_path/../Frameworks/QtOpenGL.framework/Versions/${QT_VERSION}/QtOpenGL \
+         ${APP}/Contents/MacOs/projectM-jack 
 		 
