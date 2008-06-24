a8909d4bf04336dead29c41c92620775003dbe7e
psperl <psperl@6778bc44-b910-0410-a7a0-be141de4315d>
2008-06-24 01:17:39 +0000

    thanks fatray!
    git-svn-id: https://projectm.svn.sourceforge.net/svnroot/projectm/trunk@1055 6778bc44-b910-0410-a7a0-be141de4315d

---
+++ src/projectM-engine/PipelineContext.hpp
@@ -0,0 +1,22 @@
+/*
+ * PipelineContext.hpp
+ *
+ *  Created on: Jun 22, 2008
+ *      Author: pete
+ */
+
+#ifndef PIPELINECONTEXT_HPP_
+#define PIPELINECONTEXT_HPP_
+
+class PipelineContext
+{
+public:
+	int fps;
+    float time;
+	int   frame;
+	float progress;
+
+	PipelineContext();
+};
+
+#endif /* PIPELINECONTEXT_HPP_ */
