2270d26207401c82148bec8866879532d58f091d
psperl <psperl@6778bc44-b910-0410-a7a0-be141de4315d>
2008-06-30 02:32:31 +0000

    oops
    git-svn-id: https://projectm.svn.sourceforge.net/svnroot/projectm/trunk@1076 6778bc44-b910-0410-a7a0-be141de4315d

--- src/projectM-engine/Shader.cpp
+++ src/projectM-engine/Shader.cpp
@@ -8,7 +8,6 @@
 #include "Shader.hpp"
 
 Shader::Shader()
-	: version(1), warp_version(1), composite_version(1),
-	  blur1n(1), blur2n(1), blur3n(1),
+	: blur1n(1), blur2n(1), blur3n(1),
 	  blur1x(1), blur2x(1), blur3x(1),
 	  blur1ed(1){}
