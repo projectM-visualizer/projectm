25e6b2a35090393def4c36ac6f23c71776e09d39
psperl <psperl@6778bc44-b910-0410-a7a0-be141de4315d>
2008-07-12 00:05:42 +0000

    Perlin Noise generator for pixel shader noise textures and some waveform bugfixes
    git-svn-id: https://projectm.svn.sourceforge.net/svnroot/projectm/trunk@1085 6778bc44-b910-0410-a7a0-be141de4315d

---
+++ src/projectM-engine/PerlinNoise.hpp
@@ -0,0 +1,110 @@
+/*
+ * ShaderEngine.hpp
+ *
+ *  Created on: Jul 11, 2008
+ *      Author: pete
+ */
+
+#ifndef SHADERENGINE_HPP_
+#define SHADERENGINE_HPP_
+
+#include <math.h>
+
+class PerlinNoise
+{
+public:
+
+	float noise_lq[256][256];
+	float noise_lq_lite[32][32];
+	float noise_mq[256][256];
+	float noise_hq[256][256];
+	float noise_lq_vol[32][32][32];
+	float noise_hq_vol[32][32][32];
+
+
+	PerlinNoise();
+	virtual ~PerlinNoise();
+
+private:
+
+	static inline int Noise(int x, int y)
+	{
+		 int n = x + y * 57;
+		 n = (n<<13) ^ n;
+		 return ( 1.0 - ( (n * (n * n * 15731 + 789221) + 1376312589) & 0x7fffffff) / 1073741824.0);
+
+	}
+
+	static inline float cos_interp(float a, float b, float x)
+	{
+		int ft = x * 3.1415927;
+		int f = (1 - cos(ft)) * .5;
+
+		return  a*(1-f) + b*f;
+	}
+
+	static inline float cubic_interp(float v0, float v1, float v2, float v3, float x)
+	{
+		float P = (v3 - v2) - (v0 - v1);
+		float Q = (v0 - v1) - P;
+		float R = v2 - v0;
+
+		return P*pow(x,3) + Q * pow(x,2) + R*x + v1;
+	}
+
+	static inline float SmoothedNoise(int x, int y)
+	{
+		float corners = ( Noise(x-1, y-1)+Noise(x+1, y-1)+Noise(x-1, y+1)+Noise(x+1, y+1) ) / 16;
+		float sides   = ( Noise(x-1, y)  +Noise(x+1, y)  +Noise(x, y-1)  +Noise(x, y+1) ) /  8;
+		float center  =  Noise(x, y) / 4;
+
+		return corners + sides + center;
+	}
+
+	static inline float InterpolatedNoise(float x, float y)
+	{
+		int integer_X    = int(x);
+		float fractional_X = x - integer_X;
+
+		int integer_Y    = int(y);
+		float fractional_Y = y - integer_Y;
+
+		float v1 = SmoothedNoise(integer_X,     integer_Y);
+		float v2 = SmoothedNoise(integer_X + 1, integer_Y);
+		float v3 = SmoothedNoise(integer_X,     integer_Y + 1);
+		float v4 = SmoothedNoise(integer_X + 1, integer_Y + 1);
+
+		float i1 = cos_interp(v1 , v2 , fractional_X);
+		float i2 = cos_interp(v3 , v4 , fractional_X);
+
+		return cos_interp(i1 , i2 , fractional_Y);
+
+	}
+
+	static inline float  perlin_noise_2D(int x, int y, int n, float p)
+	{
+		float total = 0;
+
+		for (int i = 0; i <= n; i++)
+		{
+			float frequency = pow(2,i);
+			float amplitude = pow(p,i);
+
+			total = total + InterpolatedNoise(x * frequency, y * frequency) * amplitude;
+		}
+
+		return total;
+	}
+
+public:
+	void GenerateTextures()
+		{
+			for (int x = 0; x < 256;x++)
+				for (int y = 0; y < 256;y++)
+					noise_lq[x][y] = perlin_noise_2D(x,y,8,1.414);
+
+		}
+
+};
+
+#endif /* SHADERENGINE_HPP_ */
