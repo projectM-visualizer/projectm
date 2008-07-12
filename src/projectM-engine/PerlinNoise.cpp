351ce26f545341af0c69f674cbc8e1f7fc6c2a7a
psperl <psperl@6778bc44-b910-0410-a7a0-be141de4315d>
2008-07-12 05:53:36 +0000

    more noise
    git-svn-id: https://projectm.svn.sourceforge.net/svnroot/projectm/trunk@1089 6778bc44-b910-0410-a7a0-be141de4315d

--- src/projectM-engine/PerlinNoise.cpp
+++ src/projectM-engine/PerlinNoise.cpp
@@ -13,22 +13,23 @@ PerlinNoise::PerlinNoise()
 {
 	for (int x = 0; x < 256;x++)
 		for (int y = 0; y < 256;y++)
-			noise_lq[x][y] = InterpolatedNoise(x,y);
+			noise_lq[x][y] = Noise(x,y);
 
 	for (int x = 0; x < 32;x++)
 		for (int y = 0; y < 32;y++)
 			noise_lq_lite[x][y] = Noise(x,y);
 
 
-	int seed = rand();
+	int seed = rand()%1000;
+	int seed2 = rand()%1000;
 
 	for (int x = 0; x < 256;x++)
 		for (int y = 0; y < 256;y++)
-			noise_mq[x][y] = perlin_noise_loop(x,y,12413,7,seed,0.5,32);
+			noise_mq[x][y] = perlin_noise_loop(x,y,1813,7,seed,0.5,32);
 
 	for (int x = 0; x < 256;x++)
 		for (int y = 0; y < 256;y++)
-			noise_hq[x][y] = perlin_noise_loop(x,y,12413,7,seed,0.5,64);
+			noise_hq[x][y] = perlin_noise_loop(x,y,6321,7,seed2,0.5,64);
 
 
 
