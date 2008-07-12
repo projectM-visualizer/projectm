1a5cc555769fa3c38205c7eb2e71791bfa8fbb2e
psperl <psperl@6778bc44-b910-0410-a7a0-be141de4315d>
2008-07-12 01:00:52 +0000

    More noise, less verbosity, and projectM.cg installation
    git-svn-id: https://projectm.svn.sourceforge.net/svnroot/projectm/trunk@1086 6778bc44-b910-0410-a7a0-be141de4315d

--- src/projectM-engine/PerlinNoise.cpp
+++ src/projectM-engine/PerlinNoise.cpp
@@ -9,8 +9,17 @@
 
 PerlinNoise::PerlinNoise()
 {
-	// TODO Auto-generated constructor stub
+	for (int x = 0; x < 256;x++)
+		for (int y = 0; y < 256;y++)
+			noise_lq[x][y] = Noise(x,y);
 
+	for (int x = 0; x < 32;x++)
+		for (int y = 0; y < 32;y++)
+			noise_lq_lite[x][y] = Noise(x,y);
+
+	for (int x = 0; x < 256;x++)
+		for (int y = 0; y < 256;y++)
+			noise_hq[x][y] = perlin_noise_2D(x,y,5,1.414);
 }
 
 PerlinNoise::~PerlinNoise()
--- src/projectM-engine/projectM.cpp
+++ src/projectM-engine/projectM.cpp
@@ -852,7 +852,7 @@ void projectM::switchPreset(std::auto_ptr<Preset> & targetPreset, PresetInputs &
 
 	// Set preset name here- event is not done because at the moment this function is oblivious to smooth/hard switches
 	renderer->setPresetName ( targetPreset->presetName() );
-
+	renderer->SetPipeline(outputs);
 
 }
 
