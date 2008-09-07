3f54c9a99d69a520b8e2459e1f49d34ea9f69ca3
w1z7ard <w1z7ard@6778bc44-b910-0410-a7a0-be141de4315d>
2008-09-07 23:10:19 +0000

    factory for compilable presets
    git-svn-id: https://projectm.svn.sourceforge.net/svnroot/projectm/personal/carm/represet@1139 6778bc44-b910-0410-a7a0-be141de4315d

---
+++ src/libprojectM/CompiledPreset.hpp
@@ -0,0 +1,38 @@
+/*
+ * Preset.hpp
+ *
+ *  Created on: Aug 5, 2008
+ *      Author: carm
+ */
+
+#ifndef PRESET_HPP_
+#define PRESET_HPP_
+
+#include <string>
+
+#include "BeatDetect.hpp"
+#include "Pipeline.hpp"
+#include "PipelineContext.hpp"
+
+
+/// A templated preset class to build different various hard coded presets and 
+/// compile them into object files to be loaded into a playlist
+template <class PipelineT>
+class CompiledPreset : public Preset {
+public:
+
+	inline CompiledPreset(const std::string & name=std::string(), 
+		const std::string & author = std::string()) : Preset(name, author) {}
+
+	virtual ~CompiledPreset() {}
+
+	inline const Pipeline & pipeline() const { return _pipeline; }
+	inline virtual void Render(const BeatDetect &music, const PipelineContext &context) {
+		_pipeline.Render(music, context);
+	}
+
+private:
+	PipelineT _pipeline;
+};
+
+#endif /* PRESET_HPP_ */
