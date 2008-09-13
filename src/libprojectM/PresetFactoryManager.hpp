c0c6a15767720ddd7aff7b1d7c58b632972740ec
w1z7ard <w1z7ard@6778bc44-b910-0410-a7a0-be141de4315d>
2008-09-13 17:27:17 +0000

    doxy comments for preset factory manager
    git-svn-id: https://projectm.svn.sourceforge.net/svnroot/projectm/personal/carm/represet@1158 6778bc44-b910-0410-a7a0-be141de4315d

--- src/libprojectM/PresetFactoryManager.hpp
+++ src/libprojectM/PresetFactoryManager.hpp
@@ -13,7 +13,7 @@
 #define __PRESET_FACTORY_MANAGER_HPP
 #include "PresetFactory.hpp"
 
-
+/// A simple exception class to strongly type all preset factory related issues
 class PresetFactoryException : public std::exception
 {
 	public:
@@ -25,21 +25,30 @@ class PresetFactoryException : public std::exception
 		std::string _message;
 };
 
+/// A manager of preset factories
 class PresetFactoryManager {
 
 	public:
 		PresetFactoryManager();
 		~PresetFactoryManager();
 
+		/// Initializes the manager with mesh sizes specified
+		/// \param gx the width of the mesh
+		/// \param gy the height of the mesh
+		/// \note This must be called before useful behavior occurs
 		void initialize(int gx, int gy);
 		
 		/// Requests a factory given a preset extension type
 		/// \param extension a string denoting the preset suffix type
 		/// \throws a PresetFactoryException if the extension is unhandled
+		/// \returns a valid preset factory associated with the extension
 		PresetFactory & factory(const std::string & extension);
 
-
+		/// Tests when an extension has been registered with a factory
+		/// \param extension the file name extension to verify
+		/// \returns true if a factory exists, false otherwise
 		bool extensionHandled(const std::string & extension) const;
+
 	private:
 		int _gx, _gy;				
 		mutable std::map<std::string, PresetFactory *> _factoryMap;
