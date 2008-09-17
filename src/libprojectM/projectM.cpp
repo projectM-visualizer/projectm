a90e7b46c8d9b100ad48ad61de068f797114a96b
w1z7ard <w1z7ard@6778bc44-b910-0410-a7a0-be141de4315d>
2008-09-17 05:02:49 +0000

    ahh, cmake 2.6 is wild stuff. hiding from it.
    git-svn-id: https://projectm.svn.sourceforge.net/svnroot/projectm/personal/carm/represet@1163 6778bc44-b910-0410-a7a0-be141de4315d

--- src/libprojectM/projectM.cpp
+++ src/libprojectM/projectM.cpp
@@ -389,13 +389,6 @@ renderer->RenderFrame(pipeline, pipelineContext);
 
 void projectM::projectM_reset()
 {
-
-	/** Default variable settings */
-
-  //	this->wvw = 512;
-  //	this->wvh = 512;
-
-	/** More other stuff */
 	this->mspf = 0;
 	this->timed = 0;
 	this->timestart = 0;
@@ -413,7 +406,6 @@ void projectM::projectM_init ( int gx, int gy, int fps, int texsize, int width, 
         timeKeeper = new TimeKeeper(_settings.presetDuration,_settings.smoothPresetDuration, _settings.easterEgg);
 
 	/** Nullify frame stash */
-
 
 	/** Initialise per-pixel matrix calculations */
 	/** We need to initialise this before the builtin param db otherwise bass/mid etc won't bind correctly */
