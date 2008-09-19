814c0c2ddf454d2da799b8b88c2604dfde0a7a63
w1z7ard <w1z7ard@6778bc44-b910-0410-a7a0-be141de4315d>
2008-09-19 22:47:04 +0000

    encapsulating native / milk files
    git-svn-id: https://projectm.svn.sourceforge.net/svnroot/projectm/personal/carm/represet@1174 6778bc44-b910-0410-a7a0-be141de4315d

--- src/libprojectM/BuiltinPresets.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef __BUILTIN_PRESETS_HPP
-#define __BUILTIN_PRESETS_HPP
-#include "CompiledPreset.hpp"
-
-
-#include "MstressJuppyDancer.hpp"
-typedef CompiledPreset<MstressJumpyDancer>  MstressJumpyDancerPreset;
-
-#endif
