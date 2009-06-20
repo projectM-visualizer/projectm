865d7f8ca8fb9e9db2b3c150f5050f0ff95b5343
w1z7ard <w1z7ard@6778bc44-b910-0410-a7a0-be141de4315d>
2009-06-20 17:18:51 +0000

    pass merge target render item as a pointer reference in merge function interface.
    git-svn-id: https://projectm.svn.sourceforge.net/svnroot/projectm/trunk@1228 6778bc44-b910-0410-a7a0-be141de4315d

--- src/libprojectM/Renderer/RenderItemMergeFunction.hpp
+++ src/libprojectM/Renderer/RenderItemMergeFunction.hpp
@@ -8,7 +8,7 @@
 #ifndef RenderItemMergeFunction_HPP_
 #define RenderItemMergeFunction_HPP_
 
-#include "Common.hpp"s
+#include "Common.hpp"
 #include "Renderable.hpp"
 #include <limits>
 #include <functional>
@@ -16,10 +16,9 @@
 
 /// Merges two render items and returns zero if they are virtually equivalent and large values
 /// when they are dissimilar. If two render items cannot be compared, NOT_COMPARABLE_VALUE is returned.
-class RenderItemMergeFunction : public std::ternary_function<const RenderItem*, const RenderItem*,
-  double ratio, RenderItem*> {
+class RenderItemMergeFunction {
 public:
-  virtual RenderItem * operator()(const RenderItem * r1, const RenderItem * r2, double ratio) const = 0;
+  virtual void operator()(const RenderItem * r1, const RenderItem * r2, RenderItem * r3, double ratio) const = 0;
   virtual TypeIdPair typeIdPair() const = 0;
 };
 
@@ -30,17 +29,17 @@ class RenderItemMerge : public RenderItemMergeFunction {
 
 protected:
 /// Override to create your own distance mergeFunction for your specified custom types.
-virtual R3 * computeMerge(const R1 * r1, const R2 * r2, double ratio) const = 0;
+virtual void computeMerge(const R1 * r1, const R2 * r2, R3 * r3, double ratio) const = 0;
 
 public:
 
-inline virtual RenderItem * operator()(const RenderItem * r1, const RenderItem * r2, double ratio) const {
+inline virtual void operator()(const RenderItem * r1, const RenderItem * r2, RenderItem * r3, double ratio) const {
 	if (supported(r1, r2))
-		return computeMerge(dynamic_cast<const R1*>(r1), dynamic_cast<const R2*>(r2));
+		return computeMerge(dynamic_cast<const R1*>(r1), dynamic_cast<const R2*>(r2), dynamic_cast<const R3*>(r3));
 	else if (supported(r2,r1))
-		return computeMerge(dynamic_cast<const R1*>(r2), dynamic_cast<const R2*>(r1));
+		return computeMerge(dynamic_cast<const R1*>(r2), dynamic_cast<const R2*>(r1), dynamic_cast<const R3*>(r3));
 	else
-		return 0;
+		return;
 }
 
 /// Returns true if and only if r1 and r2 are of type R1 and R2 respectively.
@@ -65,17 +64,19 @@ public:
 
 protected:
 
-	virtual inline Shape * computeMerge(const Shape * lhs, const Shape * rhs, double ratio) const {
-  
-	    Shape * shape  = new Shape();
-	    shape->x = (ratio*lhs->x + (1-ratio)*rhs->x)/2;
-	    shape->y = (ratio*lhs->y + (1-ratio)*rhs->y)/2;
+	virtual inline void computeMerge(const Shape * lhs, const Shape * rhs, Shape * target, double ratio) const {
+
+	    target->x = (ratio*lhs->x + (1-ratio)*rhs->x)/2;
+	    target->y = (ratio*lhs->y + (1-ratio)*rhs->y)/2;
 	    return shape;
 	}
 
 };
 
 
+/// Use as the top level merge function. It stores a map of all other
+/// merge functions, using the function that fits best with the
+/// incoming type parameters.
 class MasterRenderItemMerge : public RenderItemMerge<RenderItem> {
 
 typedef std::map<TypeIdPair, RenderItemMergeFunction*> MergeFunctionMap;
@@ -89,7 +90,7 @@ public:
 	}
 
 protected:
-	virtual inline RenderItem * computeMerge(const RenderItem * lhs, const RenderItem * rhs, double ratio) const {
+	virtual inline void computeMerge(const RenderItem * lhs, const RenderItem * rhs, RenderItem * renderItem, double ratio) const {
 
 		RenderItemMergeFunction * mergeFunction;
 
@@ -105,8 +106,9 @@ protected:
 		// If specialized mergeFunction exists, use it to get higher granularity
 		// of correctness
 		if (mergeFunction)
-			return (*mergeFunction)(lhs, rhs, ratio);
-		else return 0;
+			renderItem = (*mergeFunction)(lhs, rhs, ratio);
+		else
+			renderItem = 0;
 	}
 
 private:
