439201f5550faabcc897e503d859e26a7dff2372
w1z7ard <w1z7ard@6778bc44-b910-0410-a7a0-be141de4315d>
2009-08-03 00:39:27 +0000

    fixed qt file dialog bug.
    git-svn-id: https://projectm.svn.sourceforge.net/svnroot/projectm/trunk@1253 6778bc44-b910-0410-a7a0-be141de4315d

--- src/projectM-qt/qprojectm.hpp
+++ src/projectM-qt/qprojectm.hpp
@@ -20,7 +20,7 @@ class QProjectM : public QObject, public projectM {
 	Q_OBJECT
 
 	public:
-		QProjectM(const std::string & config_file):projectM(config_file, projectM::FLAG_DISABLE_PLAYLIST_LOAD) {} 
+		QProjectM(const std::string & config_file):projectM(config_file, projectM::FLAG_DISABLE_PLAYLIST_LOAD) {}
 
 		void presetSwitchedEvent(bool hardCut, unsigned int index) const {
 			presetSwitchedSignal(hardCut, index);
@@ -28,6 +28,7 @@ class QProjectM : public QObject, public projectM {
 	signals:
 		void presetSwitchedSignal(bool hardCut, unsigned int index) const;
 	public slots:
-			
+
+
 };
 #endif
