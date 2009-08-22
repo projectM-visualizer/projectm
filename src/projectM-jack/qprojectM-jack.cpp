7865b6f5a522eac21684307df7044f852d5b5cff
psperl <psperl@6778bc44-b910-0410-a7a0-be141de4315d>
2009-08-22 13:57:16 +0000

    GPL for jack and pulseaudio
    git-svn-id: https://projectm.svn.sourceforge.net/svnroot/projectm/trunk@1273 6778bc44-b910-0410-a7a0-be141de4315d

--- src/projectM-jack/qprojectM-jack.cpp
+++ src/projectM-jack/qprojectM-jack.cpp
@@ -3,7 +3,7 @@
  * Copyright (C)2003-2004 projectM Team
  *
  * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
+ * modify it under the terms of the GNU General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
@@ -12,12 +12,13 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public
+ * You should have received a copy of the GNU General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  * See 'LICENSE.txt' included within this release
  *
  */
+
 #include <fcntl.h>
 #include <stdlib.h>
 #include <math.h>
