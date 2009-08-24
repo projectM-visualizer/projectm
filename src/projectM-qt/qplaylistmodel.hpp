fcce3d202a6109447462f61ae01e81a9c7e487c6
w1z7ard <w1z7ard@6778bc44-b910-0410-a7a0-be141de4315d>
2009-08-24 03:37:56 +0000

    license updates in code files cand small readme changes.
    git-svn-id: https://projectm.svn.sourceforge.net/svnroot/projectm/trunk@1274 6778bc44-b910-0410-a7a0-be141de4315d

--- src/projectM-qt/qplaylistmodel.hpp
+++ src/projectM-qt/qplaylistmodel.hpp
@@ -1,23 +1,23 @@
-/***************************************************************************
- *   Copyright (C) 2007 by carm   *
- *   carmelo.piccione@gmail.com   *
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- *   This program is distributed in the hope that it will be useful,       *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
- *   GNU General Public License for more details.                          *
- *                                                                         *
- *   You should have received a copy of the GNU General Public License     *
- *   along with this program; if not, write to the                         *
- *   Free Software Foundation, Inc.,                                       *
- *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
- ***************************************************************************/
-
+/**
+ * projectM-qt -- Qt4 based projectM GUI 
+ * Copyright (C)2003-2004 projectM Team
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * See 'LICENSE.txt' included within this release
+ *
+ */
 
 #ifndef QPROJECTM_PLAYLISTMODEL_H
 #define QPROJECTM_PLAYLISTMODEL_H
