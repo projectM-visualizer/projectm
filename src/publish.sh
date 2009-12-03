ff2b12df38999dcfb621435bd5960e77e7473450
w1z7ard <w1z7ard@6778bc44-b910-0410-a7a0-be141de4315d>
2009-12-03 05:08:34 +0000

    working publish script sans automatic upload
    git-svn-id: https://projectm.svn.sourceforge.net/svnroot/projectm/trunk@1316 6778bc44-b910-0410-a7a0-be141de4315d

--- src/publish.sh
+++ src/publish.sh
@@ -1,18 +1,53 @@
 #!/bin/sh
 
-TARGET_DIR = $1
+export TARGET_DIR=$1
 
-publish {
+# Build targets
+export GZ=ON
+export BZ2=OFF
+
+#export SOURCE=ON
+
+# CG Enablement
+export CG=ON
+
+# FTGL Enablement
+export FTGL=ON
+
+# openmp Enablement
+export OPENMP=ON
+
+publish() {
+OLDDIR=`pwd`
 
 DIR=$1
-
+echo "publishing ${DIR}..."
 cd ${DIR}
 svn-clean -f
-cmake -DUSE_FTGL=ON -DUSE-CG=ON -DCMAKE_BUILD_TYPE=Release
-make package_source
+cmake -DUSE_FTGL=${FTGL} -DCPACK_SOURCE_TGZ=${GZ} -DUSE_OPENMP=${OPENMP} -DCPACK_SOURCE_TBZ2=${BZ2} -DCPACK_SOURCE_Z=OFF -DCPACK_SOURCE_TZ=OFF -DUSE_CG=${ON} -DCMAKE_BUILD_TYPE=Release > ${TARGET_DIR}/projectm-publish.log 2>&1
+
+make package_source 
 cp *.gz -f ${TARGET_DIR}
+#cp *.bz2 -f ${TARGET_DIR}
+cd ${OLDDIR}
 }
 
-
 publish "libprojectM"
+
+publish "../presets"
+publish "../presets_milkdrop_104"
+publish "../presets_milkdrop_200"
+publish "../presets_milkdrop"
+publish "../presets_projectM"
+
+publish "projectM-qt"
+publish "projectM-test"
+publish "projectM-libvisual"
+publish "projectM-libvisual-alsa"
+publish "projectM-pulseaudio"
+publish "projectM-jack"
+publish "."
+
+
+echo "[publisher] complete."
 exit
