d7847628a9a0592e802341a41a53d14ebab0c955
Mischa S <revmischa@cpan.org>
2013-08-12 18:00:09 -0700

    native preset linking, Cg linking on OSX

---
+++ src/NativePresets/CMakeFiles/RovastarFractalSpiral.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/bobo/dev/projectM-complete-2.1.0-Source/src/NativePresets/RovastarFractalSpiral.cpp" "/Users/bobo/dev/projectM-complete-2.1.0-Source/src/NativePresets/CMakeFiles/RovastarFractalSpiral.dir/RovastarFractalSpiral.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "LINUX"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/bobo/dev/projectM-complete-2.1.0-Source/src/libprojectM/Renderer/CMakeFiles/Renderer.dir/DependInfo.cmake"
+  "/Users/bobo/dev/projectM-complete-2.1.0-Source/src/libprojectM/NativePresetFactory/CMakeFiles/NativePresetFactory.dir/DependInfo.cmake"
+  "/Users/bobo/dev/projectM-complete-2.1.0-Source/src/libprojectM/CMakeFiles/projectM.dir/DependInfo.cmake"
+  "/Users/bobo/dev/projectM-complete-2.1.0-Source/src/libprojectM/MilkdropPresetFactory/CMakeFiles/MilkdropPresetFactory.dir/DependInfo.cmake"
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "src/libprojectM"
+  "src/libprojectM/Renderer"
+  "src/libprojectM/NativePresetFactory"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
