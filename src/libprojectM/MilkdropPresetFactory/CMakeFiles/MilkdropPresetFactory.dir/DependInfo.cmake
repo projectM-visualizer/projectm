a7a53a444b6d29c559f4d75f621cbf41a754f72d
Mischa S <revmischa@cpan.org>
2013-08-12 18:00:09 -0700

    native preset linking, Cg linking on OSX

---
+++ src/libprojectM/MilkdropPresetFactory/CMakeFiles/MilkdropPresetFactory.dir/DependInfo.cmake
@@ -0,0 +1,44 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/bobo/dev/projectM-complete-2.1.0-Source/src/libprojectM/MilkdropPresetFactory/BuiltinFuncs.cpp" "/Users/bobo/dev/projectM-complete-2.1.0-Source/src/libprojectM/MilkdropPresetFactory/CMakeFiles/MilkdropPresetFactory.dir/BuiltinFuncs.cpp.o"
+  "/Users/bobo/dev/projectM-complete-2.1.0-Source/src/libprojectM/MilkdropPresetFactory/BuiltinParams.cpp" "/Users/bobo/dev/projectM-complete-2.1.0-Source/src/libprojectM/MilkdropPresetFactory/CMakeFiles/MilkdropPresetFactory.dir/BuiltinParams.cpp.o"
+  "/Users/bobo/dev/projectM-complete-2.1.0-Source/src/libprojectM/MilkdropPresetFactory/CustomShape.cpp" "/Users/bobo/dev/projectM-complete-2.1.0-Source/src/libprojectM/MilkdropPresetFactory/CMakeFiles/MilkdropPresetFactory.dir/CustomShape.cpp.o"
+  "/Users/bobo/dev/projectM-complete-2.1.0-Source/src/libprojectM/MilkdropPresetFactory/CustomWave.cpp" "/Users/bobo/dev/projectM-complete-2.1.0-Source/src/libprojectM/MilkdropPresetFactory/CMakeFiles/MilkdropPresetFactory.dir/CustomWave.cpp.o"
+  "/Users/bobo/dev/projectM-complete-2.1.0-Source/src/libprojectM/MilkdropPresetFactory/Eval.cpp" "/Users/bobo/dev/projectM-complete-2.1.0-Source/src/libprojectM/MilkdropPresetFactory/CMakeFiles/MilkdropPresetFactory.dir/Eval.cpp.o"
+  "/Users/bobo/dev/projectM-complete-2.1.0-Source/src/libprojectM/MilkdropPresetFactory/Expr.cpp" "/Users/bobo/dev/projectM-complete-2.1.0-Source/src/libprojectM/MilkdropPresetFactory/CMakeFiles/MilkdropPresetFactory.dir/Expr.cpp.o"
+  "/Users/bobo/dev/projectM-complete-2.1.0-Source/src/libprojectM/MilkdropPresetFactory/Func.cpp" "/Users/bobo/dev/projectM-complete-2.1.0-Source/src/libprojectM/MilkdropPresetFactory/CMakeFiles/MilkdropPresetFactory.dir/Func.cpp.o"
+  "/Users/bobo/dev/projectM-complete-2.1.0-Source/src/libprojectM/MilkdropPresetFactory/IdlePreset.cpp" "/Users/bobo/dev/projectM-complete-2.1.0-Source/src/libprojectM/MilkdropPresetFactory/CMakeFiles/MilkdropPresetFactory.dir/IdlePreset.cpp.o"
+  "/Users/bobo/dev/projectM-complete-2.1.0-Source/src/libprojectM/MilkdropPresetFactory/InitCond.cpp" "/Users/bobo/dev/projectM-complete-2.1.0-Source/src/libprojectM/MilkdropPresetFactory/CMakeFiles/MilkdropPresetFactory.dir/InitCond.cpp.o"
+  "/Users/bobo/dev/projectM-complete-2.1.0-Source/src/libprojectM/MilkdropPresetFactory/MilkdropPreset.cpp" "/Users/bobo/dev/projectM-complete-2.1.0-Source/src/libprojectM/MilkdropPresetFactory/CMakeFiles/MilkdropPresetFactory.dir/MilkdropPreset.cpp.o"
+  "/Users/bobo/dev/projectM-complete-2.1.0-Source/src/libprojectM/MilkdropPresetFactory/MilkdropPresetFactory.cpp" "/Users/bobo/dev/projectM-complete-2.1.0-Source/src/libprojectM/MilkdropPresetFactory/CMakeFiles/MilkdropPresetFactory.dir/MilkdropPresetFactory.cpp.o"
+  "/Users/bobo/dev/projectM-complete-2.1.0-Source/src/libprojectM/MilkdropPresetFactory/Param.cpp" "/Users/bobo/dev/projectM-complete-2.1.0-Source/src/libprojectM/MilkdropPresetFactory/CMakeFiles/MilkdropPresetFactory.dir/Param.cpp.o"
+  "/Users/bobo/dev/projectM-complete-2.1.0-Source/src/libprojectM/MilkdropPresetFactory/Parser.cpp" "/Users/bobo/dev/projectM-complete-2.1.0-Source/src/libprojectM/MilkdropPresetFactory/CMakeFiles/MilkdropPresetFactory.dir/Parser.cpp.o"
+  "/Users/bobo/dev/projectM-complete-2.1.0-Source/src/libprojectM/MilkdropPresetFactory/PerFrameEqn.cpp" "/Users/bobo/dev/projectM-complete-2.1.0-Source/src/libprojectM/MilkdropPresetFactory/CMakeFiles/MilkdropPresetFactory.dir/PerFrameEqn.cpp.o"
+  "/Users/bobo/dev/projectM-complete-2.1.0-Source/src/libprojectM/MilkdropPresetFactory/PerPixelEqn.cpp" "/Users/bobo/dev/projectM-complete-2.1.0-Source/src/libprojectM/MilkdropPresetFactory/CMakeFiles/MilkdropPresetFactory.dir/PerPixelEqn.cpp.o"
+  "/Users/bobo/dev/projectM-complete-2.1.0-Source/src/libprojectM/MilkdropPresetFactory/PerPointEqn.cpp" "/Users/bobo/dev/projectM-complete-2.1.0-Source/src/libprojectM/MilkdropPresetFactory/CMakeFiles/MilkdropPresetFactory.dir/PerPointEqn.cpp.o"
+  "/Users/bobo/dev/projectM-complete-2.1.0-Source/src/libprojectM/MilkdropPresetFactory/PresetFrameIO.cpp" "/Users/bobo/dev/projectM-complete-2.1.0-Source/src/libprojectM/MilkdropPresetFactory/CMakeFiles/MilkdropPresetFactory.dir/PresetFrameIO.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "CMAKE_INSTALL_PREFIX=\"/usr/local\""
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/bobo/dev/projectM-complete-2.1.0-Source/src/libprojectM/Renderer/CMakeFiles/Renderer.dir/DependInfo.cmake"
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "src/libprojectM"
+  "src/libprojectM/Renderer"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
