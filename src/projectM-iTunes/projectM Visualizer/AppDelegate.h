4ad43b664d3ae4753c91245eda9bbf38837caf61
Mischa S <revmischa@cpan.org>
2014-08-22 22:59:33 -0700

    packaging and renaming things nicely in order to receive apple's blessing

---
+++ src/projectM-iTunes/projectM Visualizer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  projectM Visualizer
+//
+//  Created by Snooz on 8/16/14.
+//  Copyright (c) 2014 projectM. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
