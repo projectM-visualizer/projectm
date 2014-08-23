0bd27ed9c5947e427b887cb9f7e30905b703dc76
Mischa S <revmischa@cpan.org>
2014-08-22 22:59:33 -0700

    packaging and renaming things nicely in order to receive apple's blessing

---
+++ src/projectM-iTunes/projectM Visualizer/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  projectM Visualizer
+//
+//  Created by Snooz on 8/16/14.
+//  Copyright (c) 2014 projectM. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
