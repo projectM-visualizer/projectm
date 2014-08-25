003e300087709bb0a9712ae70e72e65a5bcd3686
Mischa S <revmischa@cpan.org>
2014-08-25 00:47:20 -0700

    auto-launch installer

--- src/projectM-iTunes/projectM Visualizer/AppDelegate.m
+++ src/projectM-iTunes/projectM Visualizer/AppDelegate.m
@@ -11,7 +11,7 @@
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-    
+    [self installPlugin:nil];
 }
 
 - (IBAction)installPlugin:(id)sender {
