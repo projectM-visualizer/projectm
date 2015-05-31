554aedd9cd7cbb390405ba44d4e7ac3fd2610985
Mischa S <revmischa@cpan.org>
2015-05-30 17:50:29 -0700

    got it compiling on OSX again, looking into enumeration of audio input capture sources, dropping 'for itunes' from name

--- src/projectM-iTunes/projectM Visualizer/AppDelegate.h
+++ src/projectM-iTunes/projectM Visualizer/AppDelegate.h
@@ -6,10 +6,12 @@
 //  Copyright (c) 2014 projectM. All rights reserved.
 //
 
+#import "Preferences.h"
 #import <Cocoa/Cocoa.h>
 
 @interface AppDelegate : NSObject <NSApplicationDelegate>
 
 @property (assign) IBOutlet NSWindow *window;
+@property (retain) IBOutlet Preferences *prefs;
 
 @end
