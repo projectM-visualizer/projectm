c47f426085863cd82b52b5325e71438f3c455c4d
Mischa S <revmischa@cpan.org>
2015-05-30 17:50:29 -0700

    got it compiling on OSX again, looking into enumeration of audio input capture sources, dropping 'for itunes' from name

---
+++ src/projectM-iTunes/projectM Visualizer/Preferences.m
@@ -0,0 +1,14 @@
+//
+//  Preferences.m
+//  projectM Visualizer
+//
+//  Created by Mischa on 5/30/15.
+//  Copyright (c) 2015 projectM. All rights reserved.
+//
+
+#import "Preferences.h"
+
+@implementation Preferences
+
+
+@end
