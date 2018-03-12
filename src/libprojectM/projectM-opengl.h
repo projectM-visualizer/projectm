83a8b202c86d92c0631003100b4cfb4204559362
Mischa Spiegelmock <revmischa@cpan.org>
2018-03-12 12:23:50 -0700

    opengl header

---
+++ src/libprojectM/projectM-opengl.h
@@ -0,0 +1,20 @@
+/**
+   Include appropriate OpenGL headers for this platform.
+**/
+
+#ifndef  __PROJECTM_OPENGL_H__
+#define  __PROJECTM_OPENGL_H__
+
+#ifdef __APPLE__
+# include <OpenGL/gl.h>
+#elif defined(WIN32)
+# include <windows.h>
+#else /* linux/unix/other */
+# ifdef USE_GLES1
+#  include <GLES/gl.h>
+# else
+#  include <GL/gl.h>
+# endif
+#endif
+
+#endif // __PROJECTM_OPENGL_H__
