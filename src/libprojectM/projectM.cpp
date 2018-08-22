e6f1464640b7dd43b05f4900d978cd8d3015c011
deltaoscarmike <37912794+deltaoscarmike@users.noreply.github.com>
2018-08-22 21:05:42 +0200

    Small fixes

--- src/libprojectM/projectM.cpp
+++ src/libprojectM/projectM.cpp
@@ -936,21 +936,3 @@ void projectM::getMeshSize(int *w, int *h)	{
 	*h = _settings.meshY;
 }
 
-void _check_gl_error(const char *file, int line) {
-    GLenum err (glGetError());
-    
-    while(err!=GL_NO_ERROR) {
-        string error;
-        
-        switch(err) {
-            case GL_INVALID_OPERATION:      error="INVALID_OPERATION";      break;
-            case GL_INVALID_ENUM:           error="INVALID_ENUM";           break;
-            case GL_INVALID_VALUE:          error="INVALID_VALUE";          break;
-            case GL_OUT_OF_MEMORY:          error="OUT_OF_MEMORY";          break;
-            case GL_INVALID_FRAMEBUFFER_OPERATION:  error="INVALID_FRAMEBUFFER_OPERATION";  break;
-        }
-        
-        std::cerr << "GL_" << error.c_str() <<" - "<<file<<":"<<line<<std::endl;
-        err=glGetError();
-    }
-}
