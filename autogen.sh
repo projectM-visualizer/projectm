28bc232bdd299f33c05378d260f35636c910dd73
Anton-Latukha <anton.latukha@gmail.com>
2019-10-05 22:08:30 +0300

    autogen.sh: add more portable #!, provide -e key explicitly
    This is minority, NixOS and some other distros do not have this `sh`,
    paradoxically `/usr/bin/env` path is more widespread.
    
    Also documenting `set -e`

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,7 @@
-#!/bin/sh -e
+#!/usr/bin/env sh
 
 echo "Running autoreconf... make sure you have GNU autotools available on your system"
+set -e # Drop-out from execution if error occurs
 
 autoreconf --install
 
