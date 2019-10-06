6a0c5f64381816e3cd1b05b1d6d5b7d2ed167ba5
Anton Latukha <anton.latukha@gmail.com>
2019-10-06 10:27:40 +0300

    autogen.sh: fix (#248)
    * autogen.sh: add more portable #!, provide -e key explicitly
    
    This is minority, NixOS and some other distros do not have this `sh`,
    paradoxically `/usr/bin/env` path is more widespread.
    
    Also documenting `set -e`
    
    * autogen.sh: upd output, make it portable
    
    `echo` has different behaviour, keys and possibilities on different systems.
    Example:
    ```
    echo "Line1\nLine2"
    ```
    
    Is probably parses `\n` in `macOS`, while Linux `echo` didn't, resulting
    in verbatim line.
    
    `printf` special character parsing is much more portable.
    
    Also removed the thanks "You followed directions! Great work!"
    to the person that just ran one command, which is a script that has one command
    inside and thanks the person for that.

--- autogen.sh
+++ autogen.sh
@@ -1,7 +1,9 @@
-#!/bin/sh -e
+#!/usr/bin/env sh
 
-echo "Running autoreconf... make sure you have GNU autotools available on your system"
+set -e # Drop-out from execution if error occurs
+
+printf 'Running autoreconf...'
 
 autoreconf --install
 
-echo "You followed directions! Great work!\nNow run ./configure"
+printf '\nSuccess!\nNow please run: ./configure\n'
