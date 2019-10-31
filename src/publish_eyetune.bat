5380bd2f231993c9e64f2580e93a44c36d5088ae
mancoast <RobertPancoast77@gmail.com>
2019-10-31 19:03:25 -0400

    fix presets sorting

--- src/publish_eyetune.bat
+++ src/publish_eyetune.bat
@@ -10,5 +10,16 @@ SET GITPath="C:\Program Files\Git\bin"
 set PATH=%GITPath%;%PATH%
 set REPO=%_SCRIPT_FOLDER%
 
-bash %REPO%_sync.sh
 
+REM # VC Vars #
+SET tbs_arch=x64
+SET vcvar_arg=x86_amd64
+SET VCVAR="%programfiles(x86)%\Microsoft Visual Studio\2019\Professional\VC\Auxiliary\Build\vcvarsall.bat"
+if exist %VCVAR% call %VCVAR% %vcvar_arg%
+SET VCVAR="%programfiles(x86)%\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat"
+if exist %VCVAR% call %VCVAR% %vcvar_arg%
+
+
+bash %REPO%publish_eyetune.sh
+
+pause
