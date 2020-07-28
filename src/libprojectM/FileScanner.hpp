e49720ecfa19d760fb5b6d9f9afad3dbbb43d814
Mischa Spiegelmock <mischa@mvstg.biz>
2020-07-28 22:01:56 +0300

    Preset subdirs (#385)
    Scanning textures/presets dirs for textures and scanning preset dir for presets.
    Scanning is now done recursively, so presets and textures can be organized into subdirectories instead of needing to be flattened into a single directory.
    
    On POSIX systems makes use of [ftw](https://linux.die.net/man/3/ftw) which should be relatively efficient. Otherwise falls back to recursing with `dirent` (for windows).
    
    Probably should have made an autoconf check for `ftw` instead of doing `#ifdef WIN32`. 
    
    * Scan subdirectories in presets directory
    
    * remove preset subdir config
    
    * Recursively scan for textures too, add c++-17 compatibility
    
    * Refactor directory scanning code so it's reused by texture loader and preset loader. Make cross-platform (maybe)
    
    * filescanner in makefile
    
    * extension filter for file loader
    
    * make extensions match up'
    
    * need string.h
    
    * Add FileScanner.cpp to win build (maybe)
    
    * scan all dirs
    
    * #ifndef #ifdef is def fun
    
    * bogus comment
    
    * bleh
    
    * bleh
    
    * itunes plugin with c++17
    
    * EyeTunes needs to know about the FileScanner
    
    Co-authored-by: milkdropper.com <milkdropper.com@gmail.com>
    Co-authored-by: milkdropper <59471060+milkdropper@users.noreply.github.com>

---
+++ src/libprojectM/FileScanner.hpp
@@ -0,0 +1,60 @@
+//
+//  FileScanner.hpp
+//  libprojectM
+//
+//  Cross-platform directory traversal with filtering by extension
+
+#ifndef FileScanner_hpp
+#define FileScanner_hpp
+
+#include <string>
+#include <vector>
+#include <iostream>
+#include <functional>
+#include "Common.hpp"
+#include <string.h>
+
+#ifdef WIN32
+#include "dirent.h"
+#else
+#include <fts.h>
+#endif
+
+#ifdef __unix__
+extern "C"
+{
+#include <errno.h>
+#include <dirent.h>
+}
+#endif
+
+#ifdef __APPLE__
+extern "C"
+{
+#include <errno.h>
+#include <dirent.h>
+}
+#endif
+
+
+typedef std::function<void (std::string &path, std::string &name)> ScanCallback;
+
+class FileScanner {
+public:
+    FileScanner();
+    FileScanner(std::vector<std::string> &rootDirs, std::vector<std::string> &extensions);
+
+    void scan(ScanCallback cb);
+    std::string extensionMatches(std::string &filename);
+
+private:
+    std::vector<std::string> _rootDirs;
+    std::vector<std::string> _extensions;
+
+    void scanGeneric(ScanCallback cb, const char *dir);
+    void scanPosix(ScanCallback cb);
+    void handleDirectoryError(std::string dir);
+};
+
+
+#endif /* FileScanner_hpp */
