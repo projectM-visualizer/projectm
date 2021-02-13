feb26ba6360e02653b2c49b9ab5570115327e386
Mischa Spiegelmock <me@mish.dev>
2021-02-13 17:49:47 +0200

    cleanup

--- ax_qt_main.cpp
+++
@@ -1,8 +0,0 @@
-#include "ax_qt_test.h"
-#include <qapplication.h>
-int main( int argc, char **argv )
-{
-  QApplication app( argc, argv );
-  Test t;
-  QObject::connect( &t, SIGNAL(send()), &t, SLOT(receive()) );
-}
--- src/projectM-sdl/SDL2.framework/Versions/A/Headers/SDL_revision.h
+++
@@ -1,2 +0,0 @@
-#define SDL_REVISION "hg-14525:e52d96ea04fc"
-#define SDL_REVISION_NUMBER 14525
