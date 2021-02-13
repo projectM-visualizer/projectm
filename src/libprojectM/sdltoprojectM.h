c409b4165e95657fefe7a1571050b842e775d36c
Mischa Spiegelmock <me@mish.dev>
2021-02-13 19:25:36 +0200

    wip

--- src/libprojectM/sdltoprojectM.h
+++ src/libprojectM/sdltoprojectM.h
@@ -38,11 +38,8 @@
 #include "event.h"
 
  //#include "projectM/projectM.hpp"
-#ifdef WIN32
 #include <SDL.h>
-#else
-#include <SDL2/SDL.h>
-#endif
+
 
 inline projectMEvent sdl2pmEvent( SDL_Event *event ) { \
 							
