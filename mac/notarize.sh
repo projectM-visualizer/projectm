52de3e3fc1a79bda1db14c781a97abf44e02a08f
Mischa Spiegelmock <me@mish.dev>
2021-02-23 22:09:53 +0200

    macOS installer signing works finally

--- mac/notarize.sh
+++ mac/notarize.sh
@@ -2,12 +2,10 @@
 
 DIR="$( dirname "$_" )"
 
-AUTH=--username thadwooster@gmail.com --password @keychain:AC_PASSWORD
-
-
 xcrun altool --notarize-app --primary-bundle-id "net.projectm.installer" \
       --asc-provider 'MischaSpiegelmock10100337' \
-      $AUTH --file "$DIR/../ProjectM.pkg"
+      --username thadwooster@gmail.com --password @keychain:AC_PASSWORD \
+      --file "$DIR/../ProjectM.pkg"
 
 # sleep 60
 # xcrun altool --notarization-history 0 $AUTH
