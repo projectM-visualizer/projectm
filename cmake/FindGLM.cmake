891893cda4f09c2a6013e04c1dafdb21e40f792f
Kai Blaschke <kai.blaschke@kb-dev.net>
2021-04-15 21:02:45 +0200

    Initial set of CMake files with options and some targets.

---
+++ cmake/FindGLM.cmake
@@ -0,0 +1,17 @@
+# The GLM library is header-only, so we only need to find the glm/glm.hpp header.
+find_path(GLM_INCLUDE_DIR
+        glm/glm.hpp
+        )
+
+include(FindPackageHandleStandardArgs)
+
+find_package_handle_standard_args(GLM DEFAULT_MSG GLM_INCLUDE_DIR)
+
+if(GLM_FOUND AND NOT TARGET GLM::GLM)
+    add_library(GLM::GLM UNKNOWN IMPORTED)
+
+    target_include_directories(GLM::GLM
+            INTERFACE
+            ${GLM_INCLUDE_DIR}
+            )
+endif()
