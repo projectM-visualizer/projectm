118d053051f702580bff31a753b9f5e761a0dc15
Kai Blaschke <kai.blaschke@kb-dev.net>
2021-08-16 11:38:50 +0200

    Add a dummy source file so the CMake Xcode generator creates libprojectM.a (#522)

---
+++ src/libprojectM/StaticLibDummy.cpp
@@ -0,0 +1,6 @@
+/**
+ * Dummy file for Xcode, as it doesn't create static libs with only $<TARGET_OBJECTS:...> sources.
+ * Linking this file will not add any code to the executables as the function is never referenced.
+ */
+
+void DummyFunction() {}
