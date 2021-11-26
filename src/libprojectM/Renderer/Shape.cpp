d41c017c476106c605f3a070a1b92f89a38843a6
Kai Blaschke <kai.blaschke@kb-dev.net>
2021-11-26 14:48:23 +0100

    Fixed shapecode outline rendering to match Milkdrop logic.
    Now uses anti-aliased, 1px lines and draws them four times in a rectangular offset pattern to "fake" a thicker outline.
    
    Needed to pass the actual viewport size into the render context, as the internal texture size value is useless (the target framebuffer is always as large as the viewport).

--- src/libprojectM/Renderer/Shape.cpp
+++ src/libprojectM/Renderer/Shape.cpp
@@ -18,9 +18,12 @@ Shape::Shape()
     glEnableVertexAttribArray(1);
     glEnableVertexAttribArray(2);
 
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(ShapeVertexShaderData), (void*) 0);     // Positions
-    glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, sizeof(ShapeVertexShaderData), (void*) (sizeof(float) * 2));     // Colors
-    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(ShapeVertexShaderData), (void*) (sizeof(float) * 6)); // Textures
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE,
+                          sizeof(ShapeVertexShaderData), nullptr); // Positions
+    glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE,
+                          sizeof(ShapeVertexShaderData), reinterpret_cast<void*>(sizeof(float) * 2)); // Colors
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE,
+                          sizeof(ShapeVertexShaderData), reinterpret_cast<void*>(sizeof(float) * 6)); // Textures
 
     glBindVertexArray(m_vaoID_not_texture);
     glBindBuffer(GL_ARRAY_BUFFER, m_vboID_not_texture);
@@ -28,8 +31,10 @@ Shape::Shape()
     glEnableVertexAttribArray(0);
     glEnableVertexAttribArray(1);
 
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(ShapeVertexShaderData), (void*) 0);   // points
-    glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, sizeof(ShapeVertexShaderData), (void*) (sizeof(float) * 2));     // Colors
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE,
+                          sizeof(ShapeVertexShaderData), nullptr); // Points
+    glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE,
+                          sizeof(ShapeVertexShaderData), reinterpret_cast<void*>(sizeof(float) * 2)); // Colors
 
     RenderItem::Init();
 }
@@ -46,10 +51,9 @@ Shape::~Shape()
 void Shape::InitVertexAttrib()
 {
     glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, (void*) 0);   // points
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, nullptr);   // points
     glDisableVertexAttribArray(1);
 }
-
 
 void Shape::Draw(RenderContext& context)
 {
@@ -59,7 +63,7 @@ void Shape::Draw(RenderContext& context)
 
     float temp_radius{ radius * (.707f * .707f * .707f * 1.04f) };
 
-    //Additive Drawing or Overwrite
+    // Additive Drawing or Overwrite
     if (additive == 0)
     {
         glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
@@ -110,14 +114,13 @@ void Shape::Draw(RenderContext& context)
             vertexData[i].color_b = b2;
             vertexData[i].color_a = a2 * masterAlpha;
 
-            t = (i - 1) / (float) sides;
+            t = static_cast<float>(i - 1) / static_cast<float>(sides);
             vertexData[i].tex_x = 0.5f + 0.5f * cosf(t * 3.1415927f * 2 + tex_ang + 3.1415927f * 0.25f) *
                                          (context.aspectCorrect ? context.aspectRatio : 1.0) / tex_zoom;
             vertexData[i].tex_y = 0.5f + 0.5f * sinf(t * 3.1415927f * 2 + tex_ang + 3.1415927f * 0.25f) / tex_zoom;
             vertexData[i].point_x = temp_radius * cosf(t * 3.1415927f * 2 + ang + 3.1415927f * 0.25f) *
                                     (context.aspectCorrect ? context.aspectRatio : 1.0) + xval;
             vertexData[i].point_y = temp_radius * sinf(t * 3.1415927f * 2 + ang + 3.1415927f * 0.25f) + yval;
-
         }
 
         glBindBuffer(GL_ARRAY_BUFFER, m_vboID_texture);
@@ -134,6 +137,9 @@ void Shape::Draw(RenderContext& context)
         glBindVertexArray(m_vaoID_texture);
         glDrawArrays(GL_TRIANGLE_FAN, 0, sides + 2);
         glBindVertexArray(0);
+
+        glBindTexture(GL_TEXTURE_2D, 0);
+        glBindSampler(0, 0);
     }
     else
     {//Untextured (use color values)
@@ -146,14 +152,13 @@ void Shape::Draw(RenderContext& context)
         vertexData[0].point_x = xval;
         vertexData[0].point_y = yval;
 
-
         for (int i = 1; i < sides + 2; i++)
         {
             vertexData[i].color_r = r2;
             vertexData[i].color_g = g2;
             vertexData[i].color_b = b2;
             vertexData[i].color_a = a2 * masterAlpha;
-            t = (i - 1) / (float) sides;
+            t = static_cast<float>(i - 1) / static_cast<float>(sides);
             vertexData[i].point_x = temp_radius * cosf(t * 3.1415927f * 2 + ang + 3.1415927f * 0.25f) *
                                     (context.aspectCorrect ? context.aspectRatio : 1.0) + xval;
             vertexData[i].point_y = temp_radius * sinf(t * 3.1415927f * 2 + ang + 3.1415927f * 0.25f) + yval;
@@ -174,47 +179,79 @@ void Shape::Draw(RenderContext& context)
         glBindVertexArray(0);
     }
 
-
-    floatPair* points = new float[sides + 1][2];
-
-    for (int i = 0; i < sides; i++)
+    if (border_a > 0.0f)
     {
-        t = (i - 1) / (float) sides;
-        points[i][0] = temp_radius * cosf(t * 3.1415927f * 2 + ang + 3.1415927f * 0.25f) *
-                       (context.aspectCorrect ? context.aspectRatio : 1.0) + xval;
-        points[i][1] = temp_radius * sinf(t * 3.1415927f * 2 + ang + 3.1415927f * 0.25f) + yval;
+        auto points = new float[sides + 1][2];
+
+        for (int i = 0; i < sides; i++)
+        {
+            t = (i - 1) / (float) sides;
+            points[i][0] = temp_radius * cosf(t * 3.1415927f * 2 + ang + 3.1415927f * 0.25f) *
+                           (context.aspectCorrect ? context.aspectRatio : 1.0) + xval;
+            points[i][1] = temp_radius * sinf(t * 3.1415927f * 2 + ang + 3.1415927f * 0.25f) + yval;
+        }
+
+        glUseProgram(context.programID_v2f_c4f);
+
+        glUniformMatrix4fv(context.uniform_v2f_c4f_vertex_transformation, 1, GL_FALSE,
+                           glm::value_ptr(context.mat_ortho));
+        glVertexAttrib4f(1, border_r, border_g, border_b, border_a * masterAlpha);
+        glLineWidth(1);
+        glEnable(GL_LINE_SMOOTH);
+
+        glBindVertexArray(m_vaoID);
+        glBindBuffer(GL_ARRAY_BUFFER, m_vboID);
+
+        auto iterations = thickOutline ? 4 : 1;
+
+        // Need to use +/- 1.0 here instead of 2.0 used in Milkdrop to achieve the same rendering result.
+        auto incrementX = 1.0f / static_cast<float>(context.viewportSizeX);
+        auto incrementY = 1.0f / static_cast<float>(context.viewportSizeY);
+
+        // If thick outline is used, draw the shape four times with slight offsets
+        // (top left, top right, bottom right, bottom left).
+        for (auto iteration = 0; iteration < iterations; iteration++)
+        {
+            switch (iteration)
+            {
+                case 0:
+                default:
+                    break;
+
+                case 1:
+                    for (auto j = 0; j < sides + 1; j++)
+                    {
+                        points[j][0] += incrementX;
+                    }
+                    break;
+
+                case 2:
+                    for (auto j = 0; j < sides + 1; j++)
+                    {
+                        points[j][1] += incrementY;
+                    }
+                    break;
+
+                case 3:
+                    for (auto j = 0; j < sides + 1; j++)
+                    {
+                        points[j][0] -= incrementX;
+                    }
+                    break;
+            }
+
+            glBufferData(GL_ARRAY_BUFFER, sizeof(floatPair) * (sides), points, GL_DYNAMIC_DRAW);
+            glDrawArrays(GL_LINE_LOOP, 0, sides);
+        }
+
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        glBindVertexArray(0);
+
+        delete[] points;
     }
 
-    glBindBuffer(GL_ARRAY_BUFFER, m_vboID);
-
-    glBufferData(GL_ARRAY_BUFFER, sizeof(floatPair) * (sides), NULL, GL_DYNAMIC_DRAW);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(floatPair) * (sides), points, GL_DYNAMIC_DRAW);
-
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-    glUseProgram(context.programID_v2f_c4f);
-
-    glUniformMatrix4fv(context.uniform_v2f_c4f_vertex_transformation, 1, GL_FALSE, glm::value_ptr(context.mat_ortho));
-
-    glVertexAttrib4f(1, border_r, border_g, border_b, border_a * masterAlpha);
-
-    if (thickOutline == 1)
-    {
-        glLineWidth(context.texsize < 512 ? 1 : 2 * context.texsize / 512);
-    }
-
-    glBindVertexArray(m_vaoID);
-    glDrawArrays(GL_LINE_LOOP, 0, sides);
-    glBindVertexArray(0);
-
-    glBindTexture(GL_TEXTURE_2D, 0);
-    glBindSampler(0, 0);
-
-    if (thickOutline == 1)
-    {
-        glLineWidth(context.texsize < 512 ? 1 : context.texsize / 512);
-    }
+    glDisable(GL_LINE_SMOOTH);
+    glUseProgram(0);
 
     delete[] vertexData;
-    delete[] points;
 }
