030729c9de74ecbd304fde1185a5015e725c6556
Kai Blaschke <kai.blaschke@kb-dev.net>
2021-12-07 16:21:58 +0100

    Refactored default waveform math and drawing.
    Also added Milkdrop's wave smoothing algorithm to make the waveforms look less pointy.
    
    Now very closely matches Milkdrop's math and rendering style except some very minor scaling issues.
    
    Will now render wave mode 8 (spectrum) even if not finished/enabled in the original Milkdrop. Also, Milkdrop does for some reason not use the "mystery" parameter to rotate linear waveforms (modes 6-8) even if the source suggests it. projectM will do it, but there are probably no presets that use it purposely.
    
    Also worth noting: The wave_a parameter has no effect in both Milkdrop and projectM. The wave is always drawn fully opaque.

--- src/libprojectM/Renderer/MilkdropWaveform.hpp
+++ src/libprojectM/Renderer/MilkdropWaveform.hpp
@@ -12,14 +12,15 @@
 
 enum MilkdropWaveformMode
 	{
-	  Circle = 1,
-	  RadialBlob,
-	  Blob2,
-	  Blob3,
-	  DerivativeLine,
-	  Blob5,
-	  Line,
-	  DoubleLine,
+	  Circle = 0, //!< Circular wave.
+	  XYOscillationSpiral, //!< X-Y osc. that goes around in a spiral, in time.
+	  Blob2, //!< Centered spiro (alpha constant). Aimed at not being so sound-responsive, but being very "nebula-like".
+	  Blob3, //!< Centered spiro (alpha tied to volume). Aimed at having a strong audio-visual tie-in.
+	  DerivativeLine, //!< Horizontal "script", left channel.
+	  ExplosiveHash, //!< Weird explosive complex # thingy.
+	  Line, //!< Angle-adjustable left channel, with temporal wave alignment.
+	  DoubleLine, //!< Same, except there are two channels shown.
+	  SpectrumLine, //!< Same as "Line", except using the spectrum analyzer (UNFINISHED).
 	  last // last is a placeholder to find enum size. Please ignore and leave this here.
 	};
 
@@ -27,49 +28,67 @@ enum MilkdropWaveformMode
 class MilkdropWaveform : public RenderItem
 {
 public:
+    MilkdropWaveform();
+    ~MilkdropWaveform() override;
 
+    void Draw(RenderContext &context) override;
+    void InitVertexAttrib() override;
 
-	float x;
-	float y;
+	float x{ 0.5f };
+	float y{ 0.5f };
 
-	float r;
-	float g;
-	float b;
-	float a;
+	float r{ 1.0f };
+	float g{ 0.0f };
+	float b{ 0.0f };
+	float a{ 1.0f };
 
-	float mystery;
+	float mystery{ 0.0f };
 
-    MilkdropWaveformMode mode;
+    MilkdropWaveformMode mode{ MilkdropWaveformMode::Line };
 
-	bool additive;
-	bool dots;
-	bool thick;
-	bool modulateAlphaByVolume;
-	bool maximizeColors;
-	float scale;
-	float smoothing;
+	bool additive{ false };
+	bool dots{ false };
+	bool thick{ false };
+	bool modulateAlphaByVolume{ false };
+	bool maximizeColors{ false };
+	float scale{ 10.0f };
+	float smoothing{ 0.0f };
 
-	MilkdropWaveform();
-    ~MilkdropWaveform();
-	void Draw(RenderContext &context);
-    void InitVertexAttrib();
-
-	float modOpacityStart;
-	float modOpacityEnd;
+	float modOpacityStart{ 0.0f };
+	float modOpacityEnd{ 1.0f };
 
 private:
-	float temp_a;
-	float rot;
-	float aspectScale;
-	int samples;
-	bool two_waves;
-	bool loop;
-	float wavearray[2048][2];
-	float wavearray2[2048][2];
+    static const int NumWaveformSamples{ 480 };
+
+    struct WaveformVertex
+    {
+        float x{ 0.0f };
+        float y{ 0.0f };
+    };
+
+	float temp_a{ 0.0f };
+	float rot{ 0.0f };
+	int samples{ NumWaveformSamples };
+	bool loop{ false };
+
+    WaveformVertex* wave1Vertices{ nullptr };
+    WaveformVertex* wave2Vertices{ nullptr };
 
 	void MaximizeColors(RenderContext &context);
 	void ModulateOpacityByVolume(RenderContext &context);
 	void WaveformMath(RenderContext &context);
+
+    /**
+     * @brief Does a better-than-linear smooth on a wave.
+     *
+     * Roughly doubles the number of points.
+     *
+     * @param inputVertices Pointer to an array of vertices to be smoothed.
+     * @param vertexCount Number of vertices/points in the input data.
+     * @param outputVertices Pointer to a buffer that will receive the smoothed data. Must be able to hold 2 * vertexCount vertices.
+     * @return The number of vertices in outputVertices after smoothing.
+     */
+    static int SmoothWave(const WaveformVertex* inputVertices, int vertexCount, WaveformVertex* outputVertices);
 
 };
 #endif /* MILKDROPWAVEFORM_HPP_ */
