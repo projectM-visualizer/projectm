2f10a9e0c4e576403721357a265d83bb28714f63
Kai Blaschke <kai.blaschke@kb-dev.net>
2022-02-14 11:39:06 +0100

    Removed ancient, never finished libvisual/ALSA standalone application.
    Status and purpose are unknown, if someone really wants to continue working on it, it's still in the Git history and can be outsourced into a separate repo.

--- src/projectM-libvisual-alsa/glxdriver.h
+++
@@ -1,9 +0,0 @@
-#ifndef _LV_STANDALONE_GLXDRIVER_H
-#define _LV_STANDALONE_GLXDRIVER_H
-
-#include "display.h"
-
-/* prototypes */
-SADisplayDriver *glx_driver_new (void);
-
-#endif /* _LV_STANDALONE_GLXDRIVER_H */
