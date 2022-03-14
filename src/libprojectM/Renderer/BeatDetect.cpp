626d2333fc18e0b04d3b8378751c969b97c036a4
Erik Präntare <erik.prantare@gmail.com>
2022-03-14 10:52:42 +0100

    Apply formatting

--- src/libprojectM/Renderer/BeatDetect.cpp
+++ src/libprojectM/Renderer/BeatDetect.cpp
@@ -26,42 +26,42 @@
  * you'll find it online
  */
 
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "wipemalloc.h"
 
+#include "BeatDetect.hpp"
 #include "Common.hpp"
 #include "PCM.hpp"
 #include <cmath>
-#include "BeatDetect.hpp"
 
 
-BeatDetect::BeatDetect(PCM *_pcm)
+BeatDetect::BeatDetect(PCM* _pcm)
 {
-    this->pcm=_pcm;
+    this->pcm = _pcm;
 
-    this->vol_instant=0;
-    this->vol_history=0;
-    for (unsigned y=0; y < this->vol_buffer.size(); y++)
-        this->vol_buffer[y]=0;
+    this->vol_instant = 0;
+    this->vol_history = 0;
+    for (unsigned y = 0; y < this->vol_buffer.size(); y++)
+        this->vol_buffer[y] = 0;
 
-    this->beat_buffer_pos=0;
+    this->beat_buffer_pos = 0;
 
     this->bass_instant = 0;
     this->bass_history = 0;
-    for (unsigned y=0; y < this->bass_buffer.size(); y++)
-        this->bass_buffer[y]=0;
+    for (unsigned y = 0; y < this->bass_buffer.size(); y++)
+        this->bass_buffer[y] = 0;
 
     this->mid_instant = 0;
     this->mid_history = 0;
     for (unsigned y = 0; y < this->mid_buffer.size(); y++)
-        this->mid_buffer[y]=0;
+        this->mid_buffer[y] = 0;
 
     this->treb_instant = 0;
     this->treb_history = 0;
     for (unsigned y = 0; y < this->treb_buffer.size(); y++)
-        this->treb_buffer[y]=0;
+        this->treb_buffer[y] = 0;
 
     this->treb = 0;
     this->mid = 0;
@@ -78,7 +78,6 @@ BeatDetect::BeatDetect(PCM *_pcm)
 
 BeatDetect::~BeatDetect()
 {
-
 }
 
 
@@ -92,17 +91,17 @@ void BeatDetect::reset()
     this->bass_att = 0;
     this->vol_att = 0;
     this->vol_old = 0;
-    this->vol_instant=0;
+    this->vol_instant = 0;
 }
 
 
 void BeatDetect::detectFromSamples()
 {
     vol_old = vol;
-    bass=0;
-    mid=0;
-    treb=0;
-    vol=0;
+    bass = 0;
+    mid = 0;
+    treb = 0;
+    vol = 0;
 
     float vdataL[FFT_LENGTH];
     float vdataR[FFT_LENGTH];
@@ -117,70 +116,79 @@ void BeatDetect::detectFromSamples()
 }
 
 
-void BeatDetect::getBeatVals( float samplerate, unsigned fft_length, float *vdataL, float *vdataR )
+void BeatDetect::getBeatVals(float samplerate, unsigned fft_length, float* vdataL, float* vdataR)
 {
     assert(fft_length >= 256);
-    unsigned ranges[4]  = {0, 3, 23, 255};
+    unsigned ranges[4] = {0, 3, 23, 255};
 
-    bass_instant=0;
-    for (unsigned i=ranges[0] ; i<ranges[1] ; i++)
+    bass_instant = 0;
+    for (unsigned i = ranges[0]; i < ranges[1]; i++)
         bass_instant += vdataL[i] + vdataR[i];
-    bass_history -= bass_buffer[beat_buffer_pos] * (1.0/bass_buffer.size());
+    bass_history -= bass_buffer[beat_buffer_pos] * (1.0 / bass_buffer.size());
     bass_buffer[beat_buffer_pos] = bass_instant;
-    bass_history += bass_instant * (1.0/bass_buffer.size());
+    bass_history += bass_instant * (1.0 / bass_buffer.size());
 
-    mid_instant=0;
-    for (unsigned i=ranges[1] ; i<ranges[2] ; i++)
+    mid_instant = 0;
+    for (unsigned i = ranges[1]; i < ranges[2]; i++)
         mid_instant += vdataL[i] + vdataR[i];
-    mid_history -= mid_buffer[beat_buffer_pos] * (1.0/mid_buffer.size());
+    mid_history -= mid_buffer[beat_buffer_pos] * (1.0 / mid_buffer.size());
     mid_buffer[beat_buffer_pos] = mid_instant;
-    mid_history += mid_instant * (1.0/mid_buffer.size());
+    mid_history += mid_instant * (1.0 / mid_buffer.size());
 
     treb_instant = 0;
-    for (unsigned i=ranges[2] ; i<ranges[3] ; i++)
+    for (unsigned i = ranges[2]; i < ranges[3]; i++)
         treb_instant += vdataL[i] + vdataR[i];
-    treb_history -= treb_buffer[beat_buffer_pos] * (1.0/treb_buffer.size());
+    treb_history -= treb_buffer[beat_buffer_pos] * (1.0 / treb_buffer.size());
     treb_buffer[beat_buffer_pos] = treb_instant;
-    treb_history += treb_instant * (1.0/treb_buffer.size());
+    treb_history += treb_instant * (1.0 / treb_buffer.size());
 
-    vol_instant  = (bass_instant + mid_instant + treb_instant) / 3.0f;
-    vol_history -= (vol_buffer[beat_buffer_pos]) * (1.0/vol_buffer.size());
+    vol_instant = (bass_instant + mid_instant + treb_instant) / 3.0f;
+    vol_history -= (vol_buffer[beat_buffer_pos]) * (1.0 / vol_buffer.size());
     vol_buffer[beat_buffer_pos] = vol_instant;
-    vol_history += vol_instant * (1.0/vol_buffer.size());
+    vol_history += vol_instant * (1.0 / vol_buffer.size());
 
-//    fprintf(stderr, "%6.3f %6.2f %6.3f\n", bass_history/vol_history, mid_history/vol_history, treb_history/vol_history);
+    //    fprintf(stderr, "%6.3f %6.2f %6.3f\n", bass_history/vol_history, mid_history/vol_history, treb_history/vol_history);
     bass = bass_instant / fmax(0.0001, bass_history);
-    mid  = mid_instant  / fmax(0.0001, mid_history);
+    mid = mid_instant / fmax(0.0001, mid_history);
     treb = treb_instant / fmax(0.0001, treb_history);
-    vol  = vol_instant  / fmax(0.0001, vol_history);
+    vol = vol_instant / fmax(0.0001, vol_history);
 
-    if ( projectM_isnan( treb ) ) {
+    if (projectM_isnan(treb))
+    {
         treb = 0.0;
     }
-    if ( projectM_isnan( mid ) ) {
+    if (projectM_isnan(mid))
+    {
         mid = 0.0;
     }
-    if ( projectM_isnan( bass ) ) {
+    if (projectM_isnan(bass))
+    {
         bass = 0.0;
     }
 
     treb_att = .6f * treb_att + .4f * treb;
-    mid_att  = .6f * mid_att + .4f * mid;
+    mid_att = .6f * mid_att + .4f * mid;
     bass_att = .6f * bass_att + .4f * bass;
-    vol_att =  .6f * vol_att + .4f * vol;
+    vol_att = .6f * vol_att + .4f * vol;
 
-    if (bass_att>100) bass_att=100;
-    if (bass >100) bass=100;
-    if (mid_att>100) mid_att=100;
-    if (mid >100) mid=100;
-    if (treb_att>100) treb_att=100;
-    if (treb >100) treb=100;
-    if (vol_att>100) vol_att=100;
-    if (vol>100) vol=100;
+    if (bass_att > 100)
+        bass_att = 100;
+    if (bass > 100)
+        bass = 100;
+    if (mid_att > 100)
+        mid_att = 100;
+    if (mid > 100)
+        mid = 100;
+    if (treb_att > 100)
+        treb_att = 100;
+    if (treb > 100)
+        treb = 100;
+    if (vol_att > 100)
+        vol_att = 100;
+    if (vol > 100)
+        vol = 100;
 
     beat_buffer_pos++;
-    if (beat_buffer_pos>79)
-        beat_buffer_pos=0;
+    if (beat_buffer_pos > 79)
+        beat_buffer_pos = 0;
 }
-
-
