1c436c3277ff199a55adb685ee204a867614b630
Kai Blaschke <kai.blaschke@kb-dev.net>
2022-03-24 20:12:43 +0100

    Format old parser code and fix a linker error.

--- src/libprojectM/MilkdropPresetFactory/OldParser.hpp
+++ src/libprojectM/MilkdropPresetFactory/OldParser.hpp
@@ -81,23 +81,25 @@
 
 #include <set>
 
-typedef enum {
-  UNSET_LINE_MODE,
-  PER_FRAME_LINE_MODE,
-  PER_PIXEL_LINE_MODE,
-  PER_FRAME_INIT_LINE_MODE,
-  INIT_COND_LINE_MODE,
-  CUSTOM_WAVE_PER_POINT_LINE_MODE,
-  CUSTOM_WAVE_PER_FRAME_LINE_MODE,
-  CUSTOM_WAVE_WAVECODE_LINE_MODE,
-  CUSTOM_SHAPE_SHAPECODE_LINE_MODE,
-  CUSTOM_SHAPE_PER_FRAME_LINE_MODE,
-  CUSTOM_SHAPE_PER_FRAME_INIT_LINE_MODE,
-  CUSTOM_WAVE_PER_FRAME_INIT_LINE_MODE
+typedef enum
+{
+    UNSET_LINE_MODE,
+    PER_FRAME_LINE_MODE,
+    PER_PIXEL_LINE_MODE,
+    PER_FRAME_INIT_LINE_MODE,
+    INIT_COND_LINE_MODE,
+    CUSTOM_WAVE_PER_POINT_LINE_MODE,
+    CUSTOM_WAVE_PER_FRAME_LINE_MODE,
+    CUSTOM_WAVE_WAVECODE_LINE_MODE,
+    CUSTOM_SHAPE_SHAPECODE_LINE_MODE,
+    CUSTOM_SHAPE_PER_FRAME_LINE_MODE,
+    CUSTOM_SHAPE_PER_FRAME_INIT_LINE_MODE,
+    CUSTOM_WAVE_PER_FRAME_INIT_LINE_MODE
 } line_mode_t;
 
 /** Token enumeration type */
-typedef enum {
+typedef enum
+{
     tEOL,   /* end of a line, usually a '/n' or '/r' */
     tEOF,   /* end of file */
     tLPr,   /* ( */
@@ -118,23 +120,31 @@ typedef enum {
     tSemiColon, /* ; */
     tStringTooLong, /* special token to indicate an invalid string length */
     tStringBufferFilled /* the string buffer for this line is maxed out */
-  } token_t;
+} token_t;
 
 class Test;
+
 class CustomShape;
+
 class CustomWave;
+
 class Expr;
+
 class InfixOp;
+
 class PerFrameEqn;
+
 class MilkdropPreset;
+
 class TreeExpr;
 
-class OldParser {
+class OldParser
+{
 public:
     static std::string lastLinePrefix;
     static line_mode_t line_mode;
-    static CustomWave *current_wave;
-    static CustomShape *current_shape;
+    static CustomWave* current_wave;
+    static CustomShape* current_shape;
     static int string_line_buffer_index;
     static char string_line_buffer[stringLineSize];
     static unsigned int line_count;
@@ -142,54 +152,93 @@ public:
     static int per_frame_init_eqn_count;
     static int last_custom_wave_id;
     static int last_custom_shape_id;
-    static char last_eqn_type[maxTokenSize+1];
+    static char last_eqn_type[maxTokenSize + 1];
     static int last_token_size;
     static bool tokenWrapAroundEnabled;
 
-    static Test *test();
-    static PerFrameEqn *parse_per_frame_eqn( std::istream & fs, int index,
-                                             MilkdropPreset * preset);
-    static int parse_per_pixel_eqn( std::istream & fs, MilkdropPreset * preset,
-                                    char * init_string);
-    static InitCond *parse_init_cond( std::istream & fs, char * name, MilkdropPreset * preset );
-    static int parse_preset_name( std::istream & fs, char * name );
-    static int parse_top_comment( std::istream & fs );
-    static int parse_line( std::istream & fs, MilkdropPreset * preset );
+    static Test* test();
 
-    static int get_string_prefix_len(char * string);
-    static TreeExpr * insert_gen_expr(Expr * gen_expr, TreeExpr ** root);
-    static TreeExpr * insert_infix_op(InfixOp * infix_op, TreeExpr ** root);
-    static token_t parseToken(std::istream & fs, char * string);
-    static Expr ** parse_prefix_args(std::istream & fs, int num_args, MilkdropPreset * preset);
-    static Expr * parse_infix_op(std::istream & fs, token_t token, TreeExpr * tree_expr, MilkdropPreset * preset);
-    static Expr * parse_sign_arg(std::istream & fs);
-    static int parse_float(std::istream & fs, float * float_ptr);
-    static int parse_int(std::istream & fs, int * int_ptr);
-    static int insert_gen_rec(Expr * gen_expr, TreeExpr * root);
-    static int insert_infix_rec(InfixOp * infix_op, TreeExpr * root);
-    static Expr * parse_gen_expr(std::istream & fs, TreeExpr * tree_expr, MilkdropPreset * preset);
-    static PerFrameEqn * parse_implicit_per_frame_eqn(std::istream & fs, char * param_string, int index, MilkdropPreset * preset);
-    static InitCond * parse_per_frame_init_eqn(std::istream & fs, MilkdropPreset * preset, std::map<std::string,Param*> * database);
-    static int parse_wavecode_prefix(char * token, int * id, char ** var_string);
-    static int parse_wavecode(char * token, std::istream & fs, MilkdropPreset * preset);
-    static int parse_wave_prefix(char * token, int * id, char ** eqn_string);
-    static int parse_wave_helper(std::istream & fs, MilkdropPreset * preset, int id, char * eqn_type, char * init_string);
-    static int parse_shapecode(char * eqn_string, std::istream & fs, MilkdropPreset * preset);
-    static int parse_shapecode_prefix(char * token, int * id, char ** var_string);
-    static void parse_string_block(std::istream &  fs, std::string * out_string);
-    static bool scanForComment(std::istream & fs);
-    static int parse_wave(char * eqn_string, std::istream & fs, MilkdropPreset * preset);
-    static int parse_shape(char * eqn_string, std::istream & fs, MilkdropPreset * preset);
-    static int parse_shape_prefix(char * token, int * id, char ** eqn_string);
-    static void readStringUntil(std::istream & fs, std::string * out_buffer, bool wrapAround = true, const std::set<char> & skipList = std::set<char>()) ;
+    static PerFrameEqn* parse_per_frame_eqn(std::istream& fs, int index,
+                                            MilkdropPreset* preset);
 
-    static int string_to_float(char * string, float * float_ptr);
-    static int parse_shape_per_frame_init_eqn(std::istream & fs, CustomShape * custom_shape, MilkdropPreset * preset);
-    static int parse_shape_per_frame_eqn(std::istream & fs, CustomShape * custom_shape, MilkdropPreset * preset);
-    static int parse_wave_per_frame_eqn(std::istream & fs, CustomWave * custom_wave, MilkdropPreset * preset);
-    static bool wrapsToNextLine(const std::string & str);
+    static int parse_per_pixel_eqn(std::istream& fs, MilkdropPreset* preset,
+                                   char* init_string);
+
+    static InitCond* parse_init_cond(std::istream& fs, char* name, MilkdropPreset* preset);
+
+    static int parse_preset_name(std::istream& fs, char* name);
+
+    static int parse_top_comment(std::istream& fs);
+
+    static int parse_line(std::istream& fs, MilkdropPreset* preset);
+
+    static int get_string_prefix_len(char* string);
+
+    static TreeExpr* insert_gen_expr(Expr* gen_expr, TreeExpr** root);
+
+    static TreeExpr* insert_infix_op(InfixOp* infix_op, TreeExpr** root);
+
+    static token_t parseToken(std::istream& fs, char* string);
+
+    static Expr** parse_prefix_args(std::istream& fs, int num_args, MilkdropPreset* preset);
+
+    static Expr* parse_infix_op(std::istream& fs, token_t token, TreeExpr* tree_expr, MilkdropPreset* preset);
+
+    static Expr* parse_sign_arg(std::istream& fs);
+
+    static int parse_float(std::istream& fs, float* float_ptr);
+
+    static int parse_int(std::istream& fs, int* int_ptr);
+
+    static int insert_gen_rec(Expr* gen_expr, TreeExpr* root);
+
+    static int insert_infix_rec(InfixOp* infix_op, TreeExpr* root);
+
+    static Expr* parse_gen_expr(std::istream& fs, TreeExpr* tree_expr, MilkdropPreset* preset);
+
+    static PerFrameEqn*
+    parse_implicit_per_frame_eqn(std::istream& fs, char* param_string, int index, MilkdropPreset* preset);
+
+    static InitCond*
+    parse_per_frame_init_eqn(std::istream& fs, MilkdropPreset* preset, std::map<std::string, Param*>* database);
+
+    static int parse_wavecode_prefix(char* token, int* id, char** var_string);
+
+    static int parse_wavecode(char* token, std::istream& fs, MilkdropPreset* preset);
+
+    static int parse_wave_prefix(char* token, int* id, char** eqn_string);
+
+    static int parse_wave_helper(std::istream& fs, MilkdropPreset* preset, int id, char* eqn_type, char* init_string);
+
+    static int parse_shapecode(char* eqn_string, std::istream& fs, MilkdropPreset* preset);
+
+    static int parse_shapecode_prefix(char* token, int* id, char** var_string);
+
+    static void parse_string_block(std::istream& fs, std::string* out_string);
+
+    static bool scanForComment(std::istream& fs);
+
+    static int parse_wave(char* eqn_string, std::istream& fs, MilkdropPreset* preset);
+
+    static int parse_shape(char* eqn_string, std::istream& fs, MilkdropPreset* preset);
+
+    static int parse_shape_prefix(char* token, int* id, char** eqn_string);
+
+    static void readStringUntil(std::istream& fs, std::string* out_buffer, bool wrapAround = true,
+                                const std::set<char>& skipList = std::set<char>());
+
+    static int string_to_float(char* string, float* float_ptr);
+
+    static int parse_shape_per_frame_init_eqn(std::istream& fs, CustomShape* custom_shape, MilkdropPreset* preset);
+
+    static int parse_shape_per_frame_eqn(std::istream& fs, CustomShape* custom_shape, MilkdropPreset* preset);
+
+    static int parse_wave_per_frame_eqn(std::istream& fs, CustomWave* custom_wave, MilkdropPreset* preset);
+
+    static bool wrapsToNextLine(const std::string& str);
+
 private:
-  static Expr * _parse_gen_expr(std::istream & fs, TreeExpr * tree_expr, MilkdropPreset * preset);
-  };
+    static Expr* _parse_gen_expr(std::istream& fs, TreeExpr* tree_expr, MilkdropPreset* preset);
+};
 
 #endif /** !_PARSER_H */
