3c23cb349eb69f3c7e52cd69d25a46de27ee5f4c
Erik Präntare <erik.prantare@gmail.com>
2022-04-04 16:08:54 +0200

    Apply formatting

--- src/libprojectM/Renderer/RenderItemDistanceMetric.cpp
+++ src/libprojectM/Renderer/RenderItemDistanceMetric.cpp
@@ -9,5 +9,4 @@
 
 // Assumes [0, 1] distance space because it's easy to manage with overflow
 // Underflow is obviously possible though.
-const double RenderItemDistanceMetric::NOT_COMPARABLE_VALUE
-	(1.0);
+const double RenderItemDistanceMetric::NOT_COMPARABLE_VALUE(1.0);
