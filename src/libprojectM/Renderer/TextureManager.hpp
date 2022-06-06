0e026ac7af3f3e5ca59a2d050c779e9eeb6e1539
Kai Blaschke <kai.blaschke@kb-dev.net>
2022-06-06 17:15:10 +0200

    Cleaned up TextureManager header, renamed members and fixed search path list usage.

--- src/libprojectM/Renderer/TextureManager.hpp
+++ src/libprojectM/Renderer/TextureManager.hpp
@@ -1,43 +1,40 @@
-#ifndef TextureManager_HPP
-#define TextureManager_HPP
+#pragma once
 
-#include <iostream>
-#include <string>
-#include <map>
-#include <vector>
-#include "projectM-opengl.h"
-#include "Texture.hpp"
 #include "FileScanner.hpp"
+#include "Texture.hpp"
+#include "projectM-opengl.h"
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
 
 
 class TextureManager
 {
-  std::string presetsURL;
-  std::map<std::string, Texture*> textures;
-  std::vector<Texture*> blurTextures;
-  Texture * mainTexture;
-
-  std::vector<std::string> random_textures;
-  TextureSamplerDesc loadTexture(const std::string name, const std::string imageUrl);
-  void ExtractTextureSettings(const std::string qualifiedName, GLint &_wrap_mode, GLint &_filter_mode, std::string & name);
-  std::vector<std::string> extensions;
 
 public:
-  TextureManager(std::string _presetsURL, const int texsizeX, const int texsizeY,
-                 std::string datadir = "");
-  ~TextureManager();
+    TextureManager(std::string presetPath, const int texsizeX, const int texsizeY,
+                   std::string dataPath = "");
+    ~TextureManager();
 
-  void Clear();
-  void Preload();
-  TextureSamplerDesc tryLoadingTexture(const std::string name);
-  TextureSamplerDesc getTexture(const std::string fullName, const GLenum defaultWrap, const GLenum defaultFilter);
-  const Texture * getMainTexture() const;
-  const std::vector<Texture *> & getBlurTextures() const;
+    void Clear();
+    void Preload();
+    TextureSamplerDesc tryLoadingTexture(const std::string name);
+    TextureSamplerDesc getTexture(const std::string fullName, const GLenum defaultWrap, const GLenum defaultFilter);
+    const Texture* getMainTexture() const;
+    const std::vector<Texture*>& getBlurTextures() const;
+    void updateMainTexture();
+    TextureSamplerDesc getRandomTextureName(std::string rand_name);
+    void clearRandomTextures();
 
-  void updateMainTexture();
+private:
+    TextureSamplerDesc loadTexture(const std::string name, const std::string imageUrl);
+    void ExtractTextureSettings(const std::string qualifiedName, GLint& _wrap_mode, GLint& _filter_mode, std::string& name);
 
-  TextureSamplerDesc getRandomTextureName(std::string rand_name);
-  void clearRandomTextures();
+    std::vector<std::string> m_textureDirectories;
+    std::map<std::string, Texture*> m_textures;
+    std::vector<Texture*> m_blurTextures;
+    Texture* m_mainTexture;
+    std::vector<std::string> m_randomTextures;
+    std::vector<std::string> m_extensions{".jpg", ".dds", ".png", ".tga", ".bmp", ".dib"};
 };
-
-#endif
