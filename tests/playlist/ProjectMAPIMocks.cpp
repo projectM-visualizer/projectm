bac2a7ae12bd43b5e168bbe1aad74862f13bfffd
Kai Blaschke <kai.blaschke@kb-dev.net>
2023-01-17 18:47:20 +0100

    Fix playlist tests on Windows.
    Removed dependency on projectMtarget alltogether, as the playlist tests don't need the core library at all, only a few API functions which are mocked. Also made sure the mocked functions are declared as dllexport on Windows to match the dllimport declspec in the playlist library.

--- tests/playlist/ProjectMAPIMocks.cpp
+++ tests/playlist/ProjectMAPIMocks.cpp
@@ -4,20 +4,21 @@
  */
 
 #include <libprojectM/projectM.h>
+#include <libprojectM/projectM_export.h>
 
-void projectm_set_preset_switch_requested_event_callback(projectm_handle instance,
+PROJECTM_EXPORT void projectm_set_preset_switch_requested_event_callback(projectm_handle instance,
                                                          projectm_preset_switch_requested_event callback,
                                                          void* user_data)
 {
 }
 
-void projectm_set_preset_switch_failed_event_callback(projectm_handle instance,
+PROJECTM_EXPORT void projectm_set_preset_switch_failed_event_callback(projectm_handle instance,
                                                       projectm_preset_switch_failed_event callback,
                                                       void* user_data)
 {
 }
 
-void projectm_load_preset_file(projectm_handle instance, const char* filename,
+PROJECTM_EXPORT void projectm_load_preset_file(projectm_handle instance, const char* filename,
                                bool smooth_transition)
 {
 }
