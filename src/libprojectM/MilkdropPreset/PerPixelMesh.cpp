922980f1f77a1acd11e1ed349e24f1a1b35f25e1
Kai Blaschke <kai.blaschke@kb-dev.net>
2023-02-28 19:48:37 +0100

    Per-pixel mesh implementation.

--- src/libprojectM/MilkdropPreset/PerPixelMesh.cpp
+++ src/libprojectM/MilkdropPreset/PerPixelMesh.cpp
@@ -1,48 +1,127 @@
-#include <math.h>
-#include <algorithm>
 #include "PerPixelMesh.hpp"
-#include <omptl/omptl>
-#include <omptl/omptl_algorithm>
 
-PerPixelMesh::PerPixelMesh(int _width, int _height) : width(_width), height(_height), size (_width * _height),
-        p(_width * _height, PixelPoint(0,0)),
-        p_original(_width * _height, PixelPoint(0,0)),
-        identity(_width * _height, PerPixelContext(0,0,0,0,0,0))
-		{
-		for (int j=0;j<height;j++)
-			for(int i=0;i<width;i++)
-			{
-				int index = j * width + i;
+#include "PerFrameContext.hpp"
 
-						float xval=i/(float)(width-1);
-						float yval=-((j/(float)(height-1))-1);
+#include <algorithm>
+#include <cmath>
 
-						p[index].x = xval;
-						p[index].y = yval;
-
-						p_original[index].x = xval;
-						p_original[index].y = yval;
-
-						identity[index].x= xval;
-						identity[index].y= yval;
-
-						//identity[index].x= (xval-.5)*2;
-						//identity[index].y= (yval-.5)*2;
-
-						identity[index].i= i;
-						identity[index].j= j;
-
-						identity[index].rad=hypot ( ( xval-.5 ) *2, ( yval-.5 ) *2 ) * .7071067;
-						identity[index].theta=atan2 ( ( yval-.5 ) *2 ,  ( xval-.5 ) *2  );
-			}
-		}
-
-void PerPixelMesh::Reset()
+PerPixelMesh::PerPixelMesh()
+    : RenderItem()
 {
-	omptl::copy(p_original.begin(), p_original.end(), p.begin());
 }
 
-PixelPoint::PixelPoint(float _x, float _y)
-    : x(_x), y(_y) {}
-PerPixelContext::PerPixelContext(float _x, float _y, float _rad, float _theta, int _i, int _j)
-    : x(_x), y(_y), rad(_rad), theta(_theta), i(_i), j(_j) {}
+void PerPixelMesh::InitializeMesh(const PresetState& presetState)
+{
+    if (m_gridSizeX != presetState.gridX ||
+        m_gridSizeY != presetState.gridY)
+    {
+        m_gridSizeX = presetState.gridX;
+        m_gridSizeY = presetState.gridY;
+
+        // Grid size has changed, reallocate vertex buffers
+        m_vertices.resize((m_gridSizeX + 1) * (m_gridSizeY + 1));
+        m_stripIndices.resize((m_gridSizeX + 2) * (m_gridSizeY + 2));
+        m_listIndices.resize(m_gridSizeX * m_gridSizeY * 6);
+    }
+    else if (m_viewportWidth == presetState.viewportWidth &&
+             m_viewportHeight == presetState.viewportHeight)
+    {
+        // Nothing changed, just go on to the dynamic calculation.
+        return;
+    }
+
+    // Either viewport size or mesh size changed, reinitialize the vertices.
+    int vertexIndex{0};
+    float const texelOffsetX = 0.5f / static_cast<float>(presetState.viewportWidth);
+    float const texelOffsetY = 0.5f / static_cast<float>(presetState.viewportHeight);
+
+    for (int gridY = 0; gridY < m_gridSizeY; gridY++)
+    {
+        for (int gridX = 0; gridX < m_gridSizeX; gridX++)
+        {
+            auto& vertex = m_vertices.at(vertexIndex);
+
+            vertex.x = static_cast<float>(gridX) / static_cast<float>(m_gridSizeX - 1);
+            vertex.y = -((static_cast<float>(gridY) / static_cast<float>(m_gridSizeY - 1)) - 1.0f);
+
+            // ToDo: Check if this works correctly, as it previously didn't use the aspect ratio, but some weird constant:
+            //       hypotf((vertex.x - .5f) * 2.0f, (vertex.y - .5f) * 2.0f) * .7071067f;
+            // Milkdrop uses sqrtf, but hypotf is probably safer.
+            vertex.radius = hypotf(vertex.y * static_cast<float>(presetState.aspectY),
+                                   vertex.y * static_cast<float>(presetState.aspectY));
+            if (gridY == m_gridSizeY / 2 && gridX == m_gridSizeX / 2)
+            {
+                vertex.angle = 0.0f;
+            }
+            else
+            {
+                // ToDo: Check if this works as intended with the OpenGL coordinate frame.
+                //       projectM previously used: atan2((yval - .5) * 2, (xval - .5) * 2)
+                vertex.angle = atan2f(vertex.y * static_cast<float>(presetState.aspectY),
+                                      vertex.x * static_cast<float>(presetState.aspectX));
+            }
+
+            vertex.uOrigin = vertex.x * 0.5f + 0.5f + texelOffsetX;
+            vertex.vOrigin = vertex.y * 0.5f + 0.5f + texelOffsetY;
+
+            vertexIndex++;
+        }
+    }
+
+    // Generate triangle strips for the image quadrants.
+    int vertexStripIndex{0};
+    for (int quadrant = 0; quadrant < 4; quadrant++)
+    {
+        for (int slice = 0; slice < m_gridSizeY / 2; slice++)
+        {
+            for (int gridX = 0; gridX < m_gridSizeX + 2; gridX++)
+            {
+                int xReference = gridX / 2;
+                int yReference = (gridX % 2) + slice;
+
+                if ((quadrant & 1) != 0)
+                {
+                    xReference = m_gridSizeX - xReference;
+                }
+                if ((quadrant & 2) != 0)
+                {
+                    yReference = m_gridSizeY - yReference;
+                }
+
+                m_stripIndices.at(vertexStripIndex++) = xReference + yReference * (m_gridSizeX + 1);
+            }
+        }
+    }
+
+    // Generate triangle lists for drawing the main warp mesh
+    int vertexListIndex{0};
+    for (int quadrant = 0; quadrant < 4; quadrant++)
+    {
+        for (int slice = 0; slice < m_gridSizeY / 2; slice++)
+        {
+            for (int gridX = 0; gridX < m_gridSizeX / 2; gridX++)
+            {
+                int xReference = gridX;
+                int yReference = slice;
+
+                if ((quadrant & 1) != 0)
+                {
+                    xReference = m_gridSizeX - 1 - xReference;
+                }
+                if ((quadrant & 2) != 0)
+                {
+                    yReference = m_gridSizeY - 1 - yReference;
+                }
+
+                int const vertex = xReference + yReference * (m_gridSizeX + 1);
+
+                m_listIndices.at(vertexListIndex++) = vertex;
+                m_listIndices.at(vertexListIndex++) = vertex + 1;
+                m_listIndices.at(vertexListIndex++) = vertex + m_gridSizeX + 1;
+                m_listIndices.at(vertexListIndex++) = vertex + 1;
+                m_listIndices.at(vertexListIndex++) = vertex + m_gridSizeX + 1;
+                m_listIndices.at(vertexListIndex++) = vertex + m_gridSizeX + 2;
+            }
+        }
+    }
+}
