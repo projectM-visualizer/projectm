fab2cdfe87ac7d146a895a71f23f0e7d28ba5227
Kai Blaschke <kai.blaschke@gdata.de>
2023-07-24 11:29:12 +0200

    Fix two Emscripten/GLES compile issues.

--- src/libprojectM/Renderer/Framebuffer.cpp
+++ src/libprojectM/Renderer/Framebuffer.cpp
@@ -201,7 +201,11 @@ void Framebuffer::MaskDrawBuffer(int bufferIndex, bool masked)
 {
     // Invert the flag, as "true" means the color channel *will* be written.
     auto glMasked = static_cast<GLboolean>(!masked);
+#if USE_GLES
+    glColorMask(glMasked, glMasked, glMasked, glMasked);
+#else
     glColorMaski(bufferIndex, glMasked, glMasked, glMasked, glMasked);
+#endif
 }
 
 void Framebuffer::UpdateDrawBuffers(int framebufferIndex)
