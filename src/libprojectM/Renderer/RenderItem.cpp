54617262d49b599eda55d542577886b08a67c202
kcoul <kieran.coulter@gmail.com>
2023-12-20 19:16:45 -0800

    Stop here for OpenGL ES 2.0, Sampler.cpp needs 3.0

--- src/libprojectM/Renderer/RenderItem.cpp
+++ src/libprojectM/Renderer/RenderItem.cpp
@@ -1,21 +1,39 @@
 #include "RenderItem.hpp"
 
+#if __has_include(<TargetConditionals.h>)
+#include <TargetConditionals.h>
+#define GLES_SILENCE_DEPRECATION 1
+#endif
+
 void RenderItem::Init()
 {
+#if TARGET_OS_IPHONE || TARGET_IPHONE_SIMULATOR
+    glGenVertexArraysOES(1, &m_vaoID);
+#else
     glGenVertexArrays(1, &m_vaoID);
+#endif
     glGenBuffers(1, &m_vboID);
 
+#if TARGET_OS_IPHONE || TARGET_IPHONE_SIMULATOR
+    glBindVertexArrayOES(m_vaoID);
+#else
     glBindVertexArray(m_vaoID);
+#endif
     glBindBuffer(GL_ARRAY_BUFFER, m_vboID);
-
     InitVertexAttrib();
 
+#if TARGET_OS_IPHONE || TARGET_IPHONE_SIMULATOR
+#else
     glBindVertexArray(0);
+#endif
     glBindBuffer(GL_ARRAY_BUFFER, 0);
 }
 
 RenderItem::~RenderItem()
 {
     glDeleteBuffers(1, &m_vboID);
+#if TARGET_OS_IPHONE || TARGET_IPHONE_SIMULATOR
+#else
     glDeleteVertexArrays(1, &m_vaoID);
+#endif
 }
