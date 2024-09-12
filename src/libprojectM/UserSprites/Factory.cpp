760a4c9c95ce9611e929aa1d86e497f186a7873a
Kai Blaschke <kai.blaschke@kb-dev.net>
2024-09-12 18:44:16 +0200

    Add Milkdrop user sprites

---
+++ src/libprojectM/UserSprites/Factory.cpp
@@ -0,0 +1,15 @@
+#include "Factory.hpp"
+
+namespace libprojectM {
+namespace UserSprites {
+
+Factory::Factory()
+{
+}
+
+Factory::~Factory()
+{
+}
+
+} // namespace UserSprites
+} // namespace libprojectM
