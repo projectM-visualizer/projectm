95ec5cf750e3c59faf793da20007f1d2513958cc
Kai Blaschke <kai.blaschke@kb-dev.net>
2024-12-09 16:36:01 +0100

    Implement user sprite manager and factory

--- src/libprojectM/UserSprites/Factory.hpp
+++ src/libprojectM/UserSprites/Factory.hpp
@@ -1,16 +1,19 @@
 #pragma once
 
+#include "UserSprites/Sprite.hpp"
+
+#include <memory>
+#include <string>
+
 namespace libprojectM {
 namespace UserSprites {
 
 class Factory
 {
 public:
-    Factory();
+    Factory() = delete;
 
-    virtual ~Factory();
-
-private:
+    static auto CreateSprite(const std::string& type) -> Sprite::Ptr;
 };
 
 } // namespace UserSprites
--- src/libprojectM/UserSprites/SpriteManager.hpp
+++ src/libprojectM/UserSprites/SpriteManager.hpp
@@ -1,9 +1,14 @@
 #pragma once
 
-#include "Sprite.hpp"
+#include "UserSprites/Sprite.hpp"
+
+#include <Renderer/RenderContext.hpp>
 
 #include <cstdint>
 #include <list>
+#include <set>
+#include <utility>
+#include <vector>
 
 namespace libprojectM {
 namespace UserSprites {
@@ -11,34 +16,41 @@ namespace UserSprites {
 class SpriteManager
 {
 public:
-    SpriteManager();
-
-    virtual ~SpriteManager();
+    using SpriteIdentifier = uint32_t;
 
     /**
      * @brief Spawns a new sprite.
+     * @param type The type name of the sprite.
+     * @param spriteData The sprite code/data, depending on the type.
+     * @return A unique, non-zero identifier if the sprite was successfully spawned, or zero if an error occurred.
      */
-    void Spawn();
+    auto Spawn(const std::string& type,
+               const std::string& spriteData,
+               const Renderer::RenderContext& renderContext) -> SpriteIdentifier;
 
     /**
      * @brief Draws all active sprites.
+     * @param audioData The frame audio data structure.
+     * @param renderContext The current frame's rendering context.
+     * @param outputFramebufferObject Framebuffer object the sprite will be rendered to.
+     * @param presets The active preset, plus eventually the transitioning one. Used for the burn-in effect.
      */
-    void Draw();
+    void Draw(const Audio::FrameAudioData& audioData,
+              const Renderer::RenderContext& renderContext,
+              uint32_t outputFramebufferObject,
+              Sprite::PresetList presets);
 
     /**
-     * @brief Queues a single active sprite for destruction.
+     * @brief Destroys a single active sprite.
      *
-     * The index is the 0-based index of the sprite in the list of currently active sprites, with 0 being the
-     * oldest sprite being displayed. Sprites marked for destruction will be removed when drawing the next frame.
+     * If spriteIdentifier is invalid, no action will be taken.
      *
-     * If spriteIndex is larger than the maximum index available, no action is taken.
-     *
-     * @param spriteIndex The slot index of the sprite to destroy.
+     * @param spriteIdentifier The identifier of the sprite to destroy.
      */
-    void Destroy(uint32_t spriteIndex);
+    void Destroy(SpriteIdentifier spriteIdentifier);
 
     /**
-     * @brief Marks all active sprites for destruction.
+     * @brief Destroys all active sprites.
      *
      * Sprites will be removed when drawing the next frame.
      */
@@ -48,12 +60,18 @@ public:
      * @brief Returns the current number of active sprites.
      * @return The current number of active sprites.
      */
-    auto ActiveSprites() const -> uint32_t;
+    auto ActiveSpriteCount() const -> uint32_t;
 
     /**
-     * @brief Sets the numer of available sprite slots, e.g. the number of concurrently active sprites.
+     * @brief Returns a set of identifiers for all active sprites.
+     * @return A vector with the identifiers of all active sprites.
+     */
+    auto ActiveSpriteIdentifiers() const -> std::vector<SpriteIdentifier>;
+
+    /**
+     * @brief Sets the number of available sprite slots, e.g. the number of concurrently active sprites.
      * If there are more active sprites than the newly set limit, the oldest sprites will be destroyed
-     * when drawing the next frame until the new limit it met.
+     * in order until the new limit is matched.
      * @param slots The maximum number of active sprites. 0 disables user sprites. Default is 16.
      */
     void SpriteSlots(uint32_t slots);
@@ -65,8 +83,17 @@ public:
     auto SpriteSlots() const -> uint32_t;
 
 private:
-    uint32_t m_spriteSlots{16};       //!< Max number of active sprites.
-    std::list<Sprite::Ptr> m_sprites; //!< Active sprites.
+    using SpriteIdPair = std::pair<SpriteIdentifier, Sprite::Ptr>;
+
+    /**
+     * Returns the lowest free ID, starting at 1.
+     * @return The lowest available/unused sprite ID.
+     */
+    SpriteIdentifier GetLowestFreeIdentifier();
+
+    uint32_t m_spriteSlots{16};                     //!< Max number of active sprites.
+    std::set<SpriteIdentifier> m_spriteIdentifiers; //!< Set to keep track of ordered sprite IDs.
+    std::list<SpriteIdPair> m_sprites;              //!< Active sprites with their identifier.
 };
 
 } // namespace UserSprites
