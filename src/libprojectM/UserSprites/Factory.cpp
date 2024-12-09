9266716ef6498c61bacfd0429f09057304d10a2e
Kai Blaschke <kai.blaschke@kb-dev.net>
2024-12-09 16:36:01 +0100

    Implement user sprite manager and factory

--- src/libprojectM/UserSprites/Factory.cpp
+++ src/libprojectM/UserSprites/Factory.cpp
@@ -1,15 +1,24 @@
-#include "Factory.hpp"
+#include "UserSprites/Factory.hpp"
+
+#include "UserSprites/MilkdropSprite.hpp"
+
+#include <Utils.hpp>
 
 namespace libprojectM {
 namespace UserSprites {
 
-Factory::Factory()
+auto Factory::CreateSprite(const std::string& type) -> Sprite::Ptr
 {
+    const auto lowerCaseType = Utils::ToLower(type);
+
+    if (lowerCaseType == "milkdrop")
+    {
+        return std::make_unique<MilkdropSprite>();
+    }
+
+    return {};
 }
 
-Factory::~Factory()
-{
-}
 
 } // namespace UserSprites
 } // namespace libprojectM
