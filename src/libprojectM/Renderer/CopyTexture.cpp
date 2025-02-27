e66b11066579d5b0790557b95a8e6d9342f845c5
Kai Blaschke <kai.blaschke@kb-dev.net>
2025-02-27 19:32:47 +0100

    Add texture burn-in support
    Also use shader cache in texture copier and a transformation matrix for flipping instead of flags.

--- src/libprojectM/Renderer/CopyTexture.cpp
+++ src/libprojectM/Renderer/CopyTexture.cpp
@@ -17,19 +17,11 @@ layout(location = 2) in vec2 tex_coord;
 
 out vec2 fragment_tex_coord;
 
-uniform ivec2 flip;
+uniform mat4 vertex_transformation;
 
 void main() {
-    gl_Position = vec4(position, 0.0, 1.0);
+    gl_Position = vec4(position, 0.0, 1.0) * vertex_transformation;
     fragment_tex_coord = tex_coord;
-    if (flip.x > 0)
-    {
-        fragment_tex_coord.s = 1.0 - fragment_tex_coord.s;
-    }
-    if (flip.y > 0)
-    {
-        fragment_tex_coord.t = 1.0 - fragment_tex_coord.t;
-    }
 }
 )";
 
@@ -53,13 +45,6 @@ CopyTexture::CopyTexture()
 {
     m_framebuffer.CreateColorAttachment(0, 0);
 
-    std::string vertexShader(ShaderVersion);
-    std::string fragmentShader(ShaderVersion);
-    vertexShader.append(CopyTextureVertexShader);
-    fragmentShader.append(CopyTextureFragmentShader);
-
-    m_shader.CompileProgram(vertexShader, fragmentShader);
-
     m_mesh.SetRenderPrimitiveType(Mesh::PrimitiveType::TriangleStrip);
 
     m_mesh.SetVertexCount(4);
@@ -78,7 +63,9 @@ CopyTexture::CopyTexture()
     m_mesh.Update();
 }
 
-void CopyTexture::Draw(const std::shared_ptr<class Texture>& originalTexture, bool flipVertical, bool flipHorizontal)
+void CopyTexture::Draw(ShaderCache& shaderCache,
+                       const std::shared_ptr<class Texture>& originalTexture,
+                       bool flipVertical, bool flipHorizontal)
 {
     if (originalTexture == nullptr)
     {
@@ -87,10 +74,12 @@ void CopyTexture::Draw(const std::shared_ptr<class Texture>& originalTexture, bo
 
     // Just bind the texture and draw it to the currently bound buffer.
     originalTexture->Bind(0);
-    Copy(flipVertical, flipHorizontal);
+    Copy(shaderCache, flipVertical, flipHorizontal);
 }
 
-void CopyTexture::Draw(const std::shared_ptr<class Texture>& originalTexture, const std::shared_ptr<class Texture>& targetTexture,
+void CopyTexture::Draw(ShaderCache& shaderCache,
+                       const std::shared_ptr<class Texture>& originalTexture,
+                       const std::shared_ptr<class Texture>& targetTexture,
                        bool flipVertical, bool flipHorizontal)
 {
     if (originalTexture == nullptr ||
@@ -128,7 +117,7 @@ void CopyTexture::Draw(const std::shared_ptr<class Texture>& originalTexture, co
         m_framebuffer.GetAttachment(0, TextureAttachment::AttachmentType::Color, 0)->Texture(targetTexture);
     }
 
-    Copy(flipVertical, flipHorizontal);
+    Copy(shaderCache, flipVertical, flipHorizontal);
 
     // Rebind our internal texture.
     if (targetTexture)
@@ -139,7 +128,9 @@ void CopyTexture::Draw(const std::shared_ptr<class Texture>& originalTexture, co
     Framebuffer::Unbind();
 }
 
-void CopyTexture::Draw(const std::shared_ptr<class Texture>& originalTexture, Framebuffer& framebuffer, int framebufferIndex,
+void CopyTexture::Draw(ShaderCache& shaderCache,
+                       const std::shared_ptr<class Texture>& originalTexture,
+                       Framebuffer& framebuffer, int framebufferIndex,
                        bool flipVertical, bool flipHorizontal)
 {
     if (originalTexture == nullptr                                               //
@@ -162,7 +153,7 @@ void CopyTexture::Draw(const std::shared_ptr<class Texture>& originalTexture, Fr
     // Draw from unflipped texture
     originalTexture->Bind(0);
 
-    Copy(flipVertical, flipHorizontal);
+    Copy(shaderCache, flipVertical, flipHorizontal);
 
     // Swap texture attachments
     auto tempAttachment = framebuffer.GetAttachment(framebufferIndex, TextureAttachment::AttachmentType::Color, 0);
@@ -172,6 +163,74 @@ void CopyTexture::Draw(const std::shared_ptr<class Texture>& originalTexture, Fr
     m_framebuffer.SetAttachment(0, 0, tempAttachment);
 
     Framebuffer::Unbind();
+}
+
+void CopyTexture::Draw(ShaderCache& shaderCache,
+                       const std::shared_ptr<struct Texture>& originalTexture,
+                       const std::shared_ptr<struct Texture>& targetTexture,
+                       int left, int top, int width, int height)
+{
+    if (originalTexture == nullptr ||
+        originalTexture->Empty() ||
+        targetTexture == nullptr ||
+        targetTexture->Empty() ||
+        originalTexture == targetTexture)
+    {
+        return;
+    }
+
+    UpdateTextureSize(targetTexture->Width(), targetTexture->Height());
+
+    if (m_width == 0 || m_height == 0)
+    {
+        return;
+    }
+
+    std::shared_ptr<class Texture> internalTexture;
+
+    m_framebuffer.Bind(0);
+
+    // Draw from original texture
+    originalTexture->Bind(0);
+    internalTexture = m_framebuffer.GetColorAttachmentTexture(0, 0);
+    m_framebuffer.GetAttachment(0, TextureAttachment::AttachmentType::Color, 0)->Texture(targetTexture);
+
+    Copy(shaderCache, left, top, width, height);
+
+    // Rebind our internal texture.
+    m_framebuffer.GetAttachment(0, TextureAttachment::AttachmentType::Color, 0)->Texture(internalTexture);
+
+    Framebuffer::Unbind();
+}
+
+void CopyTexture::Draw(ShaderCache& shaderCache,
+                       GLuint originalTexture,
+                       int viewportWidth, int viewportHeight,
+                       int left, int top, int width, int height)
+{
+    if (originalTexture == 0)
+    {
+        return;
+    }
+
+    if (viewportWidth == 0 || viewportHeight == 0)
+    {
+        return;
+    }
+
+    int oldWidth = m_width;
+    int oldHeight = m_height;
+
+    m_width = viewportWidth;
+    m_height = viewportHeight;
+
+    // Draw from original texture
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, originalTexture);
+    Copy(shaderCache, left, top, width, height);
+
+    m_width = oldWidth;
+    m_height = oldHeight;
 }
 
 auto CopyTexture::Texture() -> std::shared_ptr<class Texture>
@@ -193,11 +252,18 @@ void CopyTexture::UpdateTextureSize(int width, int height)
     m_framebuffer.SetSize(m_width, m_height);
 }
 
-void CopyTexture::Copy(bool flipVertical, bool flipHorizontal)
+void CopyTexture::Copy(ShaderCache& shaderCache,
+                       bool flipVertical, bool flipHorizontal)
 {
-    m_shader.Bind();
-    m_shader.SetUniformInt("texture_sampler", 0);
-    m_shader.SetUniformInt2("flip", {flipHorizontal ? 1 : 0, flipVertical ? 1 : 0});
+    glm::mat4x4 flipMatrix(1.0);
+
+    flipMatrix[0][0] = flipHorizontal ? -1.0 : 1.0;
+    flipMatrix[1][1] = flipVertical ? -1.0 : 1.0;
+
+    std::shared_ptr<Shader> shader = BindShader(shaderCache);
+
+    shader->SetUniformInt("texture_sampler", 0);
+    shader->SetUniformMat4x4("vertex_transformation", flipMatrix);
 
     m_sampler.Bind(0);
 
@@ -207,6 +273,58 @@ void CopyTexture::Copy(bool flipVertical, bool flipHorizontal)
     Mesh::Unbind();
     Sampler::Unbind(0);
     Shader::Unbind();
+}
+
+void CopyTexture::Copy(ShaderCache& shaderCache,
+                       int left, int top, int width, int height)
+{
+    glm::mat4x4 translationMatrix(1.0);
+    translationMatrix[0][0] = static_cast<float>(width) / static_cast<float>(m_width);
+    translationMatrix[1][1] = static_cast<float>(height) / static_cast<float>(m_height);
+
+    translationMatrix[3][0] = static_cast<float>(left) / static_cast<float>(m_width);
+    translationMatrix[3][1] = static_cast<float>(top) / static_cast<float>(m_height);
+
+    std::shared_ptr<Shader> shader = BindShader(shaderCache);
+
+    shader->SetUniformInt("texture_sampler", 0);
+    shader->SetUniformMat4x4("vertex_transformation", translationMatrix);
+
+    m_sampler.Bind(0);
+
+    m_mesh.Draw();
+
+    Mesh::Unbind();
+    Sampler::Unbind(0);
+    Shader::Unbind();
+}
+
+std::shared_ptr<Shader> CopyTexture::BindShader(ShaderCache& shaderCache)
+{
+    auto shader = m_shader.lock();
+
+    if (!shader)
+    {
+        shader = shaderCache.Get("copy_texture");
+    }
+
+    if (!shader)
+    {
+        std::string vertexShader(ShaderVersion);
+        std::string fragmentShader(ShaderVersion);
+        vertexShader.append(CopyTextureVertexShader);
+        fragmentShader.append(CopyTextureFragmentShader);
+
+        shader = std::make_shared<Shader>();
+        shader->CompileProgram(vertexShader, fragmentShader);
+
+        m_shader = shader;
+        shaderCache.Insert("copy_texture", shader);
+    }
+
+    shader->Bind();
+
+    return shader;
 }
 
 } // namespace Renderer
