f62adf71c038c81af916a3664f10c69e74e656bf
Noah Cohn <noahc42@gmail.com>
2025-05-20 08:28:43 -0700

    Update projectM_emscripten.cpp

--- projectM_emscripten.cpp
+++ projectM_emscripten.cpp
@@ -705,7 +705,7 @@ app_data.playlist = playlist;
     projectm_set_soft_cut_duration(pm, 17);
     projectm_playlist_set_shuffle(playlist,true);
     projectm_set_preset_switch_failed_event_callback(pm, &_on_preset_switch_failed, nullptr);
-    projectm_set_preset_switch_requested_event_callback(pm, &on_preset_switch_requested, &app_data);
+    projectm_set_preset_switch_requested_event_callback(pm, &on_preset_switch_requested, nullptr);
     printf("projectM initialized\n");
     return 0;
 }
@@ -713,6 +713,7 @@ app_data.playlist = playlist;
 void add_preset_path(){
 const char * loc="/presets/";
 projectm_playlist_add_path(app_data.playlist,loc,true,true);
+projectm_playlist_connect(app_data.playlist,app_data.projectm_engine);
 printf("Added /presets/ to playlist successfully.\n");
 }
 
